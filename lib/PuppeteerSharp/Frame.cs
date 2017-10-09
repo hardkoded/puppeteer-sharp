87439e17c5f7cfd7ac7b8563b9e89d15a6624ff4
Darío Kondratiuk <dariokondratiuk@gmail.com>
2017-10-08 22:26:21 -0300

    Frame partial implementation

---
+++ lib/PuppeteerSharp/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using PuppeteerSharp.Input;
+
+namespace PuppeteerSharp
+{
+    public class Frame
+    {
+        private Session _client;
+        private Mouse _mouse;
+        private Touchscreen _touchscreen;
+        private Frame _parentFrame;
+        private string _url = string.Empty;
+        private string _id;
+        private string _defaultContextId = "<not-initialized>";
+
+        public Frame(Session client, Mouse mouse, Touchscreen touchscreen, Frame parentFrame, string frameId)
+        {
+            _client = client;
+            _mouse = mouse;
+            _touchscreen = touchscreen;
+            _id = frameId;
+            _parentFrame = parentFrame;
+
+            if(parentFrame != null)
+            {
+                _parentFrame.ChildFrames.Add(this);
+            }
+        }
+
+		public List<Frame> ChildFrames = new List<Frame>();
+
+
+	}
+}
---
+++ lib/PuppeteerSharp/FrameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using PuppeteerSharp.Input;
+
+namespace PuppeteerSharp
+{
+    public class FrameManager
+    {
+        private Session _client;
+        private Mouse _mouse;
+        private Touchscreen _touchscreen;
+        private Dictionary<string, Frame> _frames;
+
+        public FrameManager()
+        {
+        }
+    }
+}
