5f1ec97908febc8510000dc28e131bd1e3333a3f
Darío Kondratiuk <dariokondratiuk@gmail.com>
2017-12-27 09:11:44 -0300

    Request.Respond method

--- lib/PuppeteerSharp/Request.cs
+++ lib/PuppeteerSharp/Request.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PuppeteerSharp
@@ -22,6 +24,21 @@ namespace PuppeteerSharp
         private string _method;
         private object _postData;
 
+        private readonly Dictionary<string, string> _errorReasons = new Dictionary<string, string>
+        {
+            {"aborted", "Aborted"},
+            {"accessdenied", "AccessDenied"},
+            {"addressunreachable", "AddressUnreachable"},
+            {"connectionaborted", "ConnectionAborted"},
+            {"connectionclosed", "ConnectionClosed"},
+            {"connectionfailed", "ConnectionFailed"},
+            {"connectionrefused", "ConnectionRefused"},
+            {"connectionreset", "ConnectionReset"},
+            {"internetdisconnected", "InternetDisconnected"},
+            {"namenotresolved", "NameNotResolved"},
+            {"timedout", "TimedOut"},
+            {"failed", "Failed"},
+        };
         #endregion
 
         public Request(Session client, string requestId, string interceptionId, bool allowInterception, string url,
@@ -69,7 +86,7 @@ namespace PuppeteerSharp
                     {"headers", overrides.Headers}
                 });
             }
-            catch(Exception)
+            catch (Exception)
             {
                 // In certain cases, protocol will return error if the request was already canceled
                 // or the page was closed. We should tolerate these errors
@@ -78,9 +95,80 @@ namespace PuppeteerSharp
         }
 
 
-        public async Task<Response> Respond()
+        public async Task Respond()
         {
-            
+            if (_url.StartsWith("data:", StringComparison.Ordinal))
+            {
+                return;
+            }
+
+            Contract.Requires(_allowInterception, "Request interception is not enabled!");
+            Contract.Requires(_interceptionHandled, "Request is already handled!");
+
+            _interceptionHandled = true;
+
+            //In puppeteer this is a buffer but as I don't know the real implementation yet
+            //I will consider this a string
+            var responseBody = _response.Body;
+            var responseHeaders = new Dictionary<string, object>();
+
+            if (_response.Headers != null)
+            {
+                foreach (var keyValue in _response.Headers)
+                {
+                    responseHeaders[keyValue.Key] = keyValue.Value;
+                }
+            }
+
+            if (_response.ContentType != null)
+            {
+                responseHeaders["content-type"] = _response.ContentType;
+            }
+
+            if (!string.IsNullOrEmpty(responseBody) && !responseHeaders.ContainsKey("content-length"))
+            {
+                responseHeaders["content-length"] = responseBody.Length;
+            }
+
+            var statusCode = Response.Status ?? HttpStatusCode.Accepted;
+            var statusText = statusCode.ToString();
+            var statusLine = $"HTTP / 1.1${(int)statusCode} ${statusText}";
+
+            var text = new StringBuilder(statusLine + "\n");
+
+            foreach (var header in responseHeaders)
+            {
+                text.AppendLine($"{header.Key}: {header.Value}");
+            }
+            text.AppendLine(string.Empty);
+
+            //This is a buffer in puppeteer but I don't know the final implementation here
+            var responseBuffer = text.ToString();
+
+            if (!string.IsNullOrEmpty(responseBody))
+            {
+                responseBuffer += responseBody;
+            }
+
+            try
+            {
+                await _client.SendAsync("Network.continueInterceptedRequest", new Dictionary<string, object>
+                {
+                    {"interceptionId", _interceptionId},
+                    {"rawResponse", responseBuffer}
+                });
+            }
+            catch (Exception)
+            {
+                // In certain cases, protocol will return error if the request was already canceled
+                // or the page was closed. We should tolerate these errors
+                //TODO: Choose log mechanism
+            }
+        }
+
+        public async Task Abort(string errorCode = "failed")
+        {
+
         }
         #endregion
 
--- lib/PuppeteerSharp/Response.cs
+++ lib/PuppeteerSharp/Response.cs
@@ -1,7 +1,13 @@
-﻿namespace PuppeteerSharp
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace PuppeteerSharp
 {
     public class Response
     {
-
+        public string Body { get; internal set; }
+        public Dictionary<string, object> Headers { get; internal set; }
+        public string ContentType { get; internal set; }
+        public HttpStatusCode? Status { get; internal set; }
     }
 }
