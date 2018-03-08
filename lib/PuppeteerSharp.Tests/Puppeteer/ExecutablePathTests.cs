ffc9b5aea28227c7ac3b9317b3c324b0baa5fc8e
Meir Blachman <meirblachman@gmail.com>
2018-03-08 20:38:12 +0200

    added Puppeteer.executablePath tests

---
+++ lib/PuppeteerSharp.Tests/Puppeteer/ExecutablePathTests.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.Puppeteer
+{
+    public class ExecutablePathTests
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            var executablePath = PuppeteerSharp.Puppeteer.GetExecutablePath();
+            Assert.True(File.Exists(executablePath));
+        }
+    }
+}
--- lib/PuppeteerSharp/Puppeteer.cs
+++ lib/PuppeteerSharp/Puppeteer.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace PuppeteerSharp
 {
     public class Puppeteer
     {
         public static string[] DefaultArgs => Launcher.DefaultArgs;
+
+        public static string GetExecutablePath()
+        {
+            var downloader = Downloader.CreateDefault();
+            var revisionInfo = downloader.RevisionInfo(Downloader.CurrentPlatform, Downloader.DefaultRevision);
+            return revisionInfo.ExecutablePath;
+        }
 
         public static async Task<Browser> LaunchAsync(LaunchOptions options, int chromiumRevision)
         {
