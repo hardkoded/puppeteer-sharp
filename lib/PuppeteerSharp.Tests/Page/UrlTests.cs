1f73f5e361b3d884fd9b4a999057d54558677987
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-03-12 08:25:11 -0300

    Page.Url tests

---
+++ lib/PuppeteerSharp.Tests/Page/UrlTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.Page
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class UrlTests : PuppeteerBaseTest
+    {
+        [Fact]
+        public async Task ShouldWork()
+        {
+            using (var page = await Browser.NewPageAsync())
+            {
+                Assert.Equal(TestConstants.AboutBlank, page.Url);
+                await page.GoToAsync(TestConstants.EmptyPage);
+                Assert.Equal(TestConstants.EmptyPage, page.Url);
+            }
+        }
+    }
+}
