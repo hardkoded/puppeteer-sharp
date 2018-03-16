bfe5634382a078d86f8c418f23aeafc897ef40b6
Meir Blachman <meirblachman@gmail.com>
2018-03-16 16:31:05 +0200

    cr fix

--- lib/PuppeteerSharp/ContextPayload.cs
+++ lib/PuppeteerSharp/ContextPayload.cs
@@ -2,21 +2,14 @@
 {
     public class ContextPayload
     {
-
         public ContextPayload(dynamic context)
         {
             Id = context.id;
-            AuxData = context.auxData.ToObject<AuxData>();
+            AuxData = context.auxData.ToObject<ContextPayloadAuxData>();
         }
 
         public int Id { get; internal set; }
 
-        public AuxData AuxData { get; internal set; }
-    }
-
-    public class AuxData
-    {
-        public string FrameId { get; set; }
-        public bool IsDefault { get; set; }
+        public ContextPayloadAuxData AuxData { get; internal set; }
     }
 }
