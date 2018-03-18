714a43c8065f3026f73b295851e48831749c6107
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-03-18 13:44:49 -0300

    Self review

--- lib/PuppeteerSharp/EvaluateExceptionDetails.cs
+++ lib/PuppeteerSharp/EvaluateExceptionDetails.cs
@@ -8,6 +8,7 @@ namespace PuppeteerSharp
         public EvaluateExceptionInfo Exception { get; set; }
         [JsonProperty("text")]
         public string Text { get; internal set; }
+        [JsonProperty("stackTrace")]
         public EvaluateExceptionStackTrace StackTrace { get; internal set; }
     }
 }
