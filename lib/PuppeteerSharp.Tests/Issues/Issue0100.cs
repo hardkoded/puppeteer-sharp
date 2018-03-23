a2bafb93f8d378609b21e1875fbd02cadbdf49ef
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-03-23 08:27:06 -0300

    Test fixes

--- lib/PuppeteerSharp.Tests/Issues/Issue0100.cs
+++ lib/PuppeteerSharp.Tests/Issues/Issue0100.cs
@@ -4,18 +4,12 @@ using Xunit;
 
 namespace PuppeteerSharp.Tests.Issues
 {
+    [CollectionDefinition("PuppeteerLoaderFixture collection")]
     public class Issue0100 : PuppeteerBaseTest
     {
-        public Issue0100()
-        {
-            Downloader.CreateDefault().DownloadRevisionAsync(TestConstants.ChromiumRevision).GetAwaiter().GetResult();
-        }
-
         [Fact]
         public async Task PdfWundergroundShouldWork()
         {
-            using (var browser = PuppeteerSharp.Puppeteer.LaunchAsync(TestConstants.DefaultBrowserOptions(),
-                                                                     TestConstants.ChromiumRevision))
             using (var page = await Browser.NewPageAsync())
             {
                 var response = await page.GoToAsync("https://www.wunderground.com/weather/be/antwerp");
@@ -27,8 +21,6 @@ namespace PuppeteerSharp.Tests.Issues
         [Fact]
         public async Task PdfDarkskyShouldWork()
         {
-            using (var browser = PuppeteerSharp.Puppeteer.LaunchAsync(TestConstants.DefaultBrowserOptions(),
-                                                                     TestConstants.ChromiumRevision))
             using (var page = await Browser.NewPageAsync())
             {
                 await page.GoToAsync("https://darksky.net/forecast/51.2211,4.3997/si12/en");
