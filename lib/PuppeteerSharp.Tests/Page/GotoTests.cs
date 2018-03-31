842c860e6dc706cf789f647ffc3ff25236179eec
Meir Blachman <meirblachman@gmail.com>
2018-03-31 22:14:06 +0300

    more tests

--- lib/PuppeteerSharp.Tests/Page/GotoTests.cs
+++ lib/PuppeteerSharp.Tests/Page/GotoTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace PuppeteerSharp.Tests.Page
@@ -13,6 +15,29 @@ namespace PuppeteerSharp.Tests.Page
             {
                 var response = await page.GoToAsync(TestConstants.AboutBlank);
                 Assert.Null(response);
+            }
+        }
+
+        [Theory]
+        [InlineData("domcontentloaded")]
+        [InlineData("networkidle0")]
+        [InlineData("networkidle2")]
+        public async Task ShouldNavigateToEmptyPage(string waitUntil)
+        {
+            using (var page = await Browser.NewPageAsync())
+            {
+                var response = await page.GoToAsync(TestConstants.EmptyPage, new NavigationOptions { WaitUntil =  new[] { waitUntil } });
+                Assert.Equal(HttpStatusCode.OK, response.Status);
+            }
+        }
+
+        [Fact]
+        public async Task ShouldFailWhenNavigatingToBadUrl()
+        {
+            using (var page = await Browser.NewPageAsync())
+            {
+                var exception = await Assert.ThrowsAnyAsync<Exception>(async () => await page.GoToAsync("asdfasdf"));
+                Assert.Contains("Cannot navigate to invalid URL", exception.Message);
             }
         }
     }
--- lib/PuppeteerSharp/NavigationOptions.cs
+++ lib/PuppeteerSharp/NavigationOptions.cs
@@ -3,6 +3,6 @@
     public class NavigationOptions
     {
         public int? Timeout { get; internal set; }
-        public string[] WaitUntil { get; internal set; }
+        public string[] WaitUntil { get; set; }
     }
 }
