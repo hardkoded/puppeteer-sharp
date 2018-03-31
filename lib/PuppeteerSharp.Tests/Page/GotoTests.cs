976623854c3c5e202b00887140415caaa50c0b9c
Meir Blachman <meirblachman@gmail.com>
2018-03-31 21:23:24 +0300

    added first test

---
+++ lib/PuppeteerSharp.Tests/Page/GotoTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.Page
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class GotoTests : PuppeteerBaseTest
+    {
+        [Fact]
+        public async Task ShouldNavigateToAboutBlank()
+        {
+            using (var page = await Browser.NewPageAsync())
+            {
+                var response = await page.GoToAsync(TestConstants.AboutBlank);
+                Assert.Null(response);
+            }
+        }
+    }
+}
