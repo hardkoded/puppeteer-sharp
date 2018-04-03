f193f5e2f17c5183d58c5be72f3d4bfde24f922f
Meir Blachman <meirblachman@gmail.com>
2018-04-03 20:43:01 +0300

    EvaluateTests

--- lib/PuppeteerSharp.Tests/Page/EvaluateTests.cs
+++ lib/PuppeteerSharp.Tests/Page/EvaluateTests.cs
@@ -6,18 +6,15 @@ using Xunit;
 namespace PuppeteerSharp.Tests.Page
 {
     [Collection("PuppeteerLoaderFixture collection")]
-    public class EvaluateTests : PuppeteerBaseTest
+    public class EvaluateTests : PuppeteerPageBaseTest
     {
         [Theory]
         [InlineData("1 + 5;", 6)] //ShouldAcceptSemiColons
         [InlineData("2 + 5\n// do some math!'", 7)] //ShouldAceptStringComments
         public async Task BasicIntExressionEvaluationTest(string script, object expected)
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                var result = await page.EvaluateExpressionAsync<int>(script);
-                Assert.Equal(expected, result);
-            }
+            var result = await Page.EvaluateExpressionAsync<int>(script);
+            Assert.Equal(expected, result);
         }
 
         [Theory]
@@ -25,11 +22,8 @@ namespace PuppeteerSharp.Tests.Page
         [InlineData("() => Promise.resolve(8 * 7)", 56)] //ShouldAwaitPromise
         public async Task BasicIntFunctionEvaluationTest(string script, object expected)
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                var result = await page.EvaluateFunctionAsync<int>(script);
-                Assert.Equal(expected, result);
-            }
+            var result = await Page.EvaluateFunctionAsync<int>(script);
+            Assert.Equal(expected, result);
         }
 
         [Fact]
@@ -37,44 +31,35 @@ namespace PuppeteerSharp.Tests.Page
         {
             Task<int> frameEvaluation = null;
 
-            using (var page = await Browser.NewPageAsync())
+            Page.FrameNavigated += (sender, e) =>
             {
-                page.FrameNavigated += (sender, e) =>
-                {
-                    frameEvaluation = e.Frame.EvaluateFunctionAsync<int>("() => 6 * 7");
-                };
+                frameEvaluation = e.Frame.EvaluateFunctionAsync<int>("() => 6 * 7");
+            };
 
-                await page.GoToAsync(TestConstants.EmptyPage);
-                Assert.Equal(42, await frameEvaluation);
-            }
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            Assert.Equal(42, await frameEvaluation);
         }
 
         [Fact]
         public async Task ShouldRejectPromiseWithExeption()
         {
-            using (var page = await Browser.NewPageAsync())
+            var exception = await Assert.ThrowsAsync<EvaluationFailedException>(() =>
             {
-                var exception = await Assert.ThrowsAsync<EvaluationFailedException>(() =>
-                {
-                    return page.EvaluateFunctionAsync<object>("() => not.existing.object.property");
-                });
+                return Page.EvaluateFunctionAsync<object>("() => not.existing.object.property");
+            });
 
-                Assert.Contains("not is not defined", exception.Message);
-            }
+            Assert.Contains("not is not defined", exception.Message);
         }
 
         [Fact]
         public async Task SouldReturnComplexObjects()
         {
-            using (var page = await Browser.NewPageAsync())
+            dynamic obj = new
             {
-                dynamic obj = new
-                {
-                    foo = "bar!"
-                };
-                dynamic result = await page.EvaluateFunctionAsync("a => a", obj);
-                Assert.Equal("bar!", result.foo.ToString());
-            }
+                foo = "bar!"
+            };
+            dynamic result = await Page.EvaluateFunctionAsync("a => a", obj);
+            Assert.Equal("bar!", result.foo.ToString());
         }
 
         [Theory]
@@ -84,42 +69,30 @@ namespace PuppeteerSharp.Tests.Page
         [InlineData("() => -Infinity", double.NegativeInfinity)] //ShouldReturnNegativeInfinty
         public async Task BasicEvaluationTest(string script, object expected)
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                dynamic result = await page.EvaluateFunctionAsync(script);
-                Assert.Equal(expected, result);
-            }
+            dynamic result = await Page.EvaluateFunctionAsync(script);
+            Assert.Equal(expected, result);
         }
 
         [Fact]
         public async Task ShouldAcceptNullAsOneOfMultipleParameters()
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                bool result = await page.EvaluateFunctionAsync<bool>("(a, b) => Object.is(a, null) && Object.is(b, 'foo')", null, "foo");
-                Assert.True(result);
-            }
+            bool result = await Page.EvaluateFunctionAsync<bool>("(a, b) => Object.is(a, null) && Object.is(b, 'foo')", null, "foo");
+            Assert.True(result);
         }
 
         [Fact]
         public async Task ShouldProperlyIgnoreUndefinedFields()
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                var result = await page.EvaluateFunctionAsync<Dictionary<string, object>>("() => ({a: undefined})");
-                Assert.Empty(result);
-            }
+            var result = await Page.EvaluateFunctionAsync<Dictionary<string, object>>("() => ({a: undefined})");
+            Assert.Empty(result);
         }
 
         [Fact]
         public async Task ShouldProperlySerializeNullFields()
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                var result = await page.EvaluateFunctionAsync<Dictionary<string, object>>("() => ({a: null})");
-                Assert.True(result.ContainsKey("a"));
-                Assert.Null(result["a"]);
-            }
+            var result = await Page.EvaluateFunctionAsync<Dictionary<string, object>>("() => ({a: null})");
+            Assert.True(result.ContainsKey("a"));
+            Assert.Null(result["a"]);
         }
     }
 }
