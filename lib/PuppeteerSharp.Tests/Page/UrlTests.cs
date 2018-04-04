209e0b3bdf0d37485dd515960bdc2e89e7578f87
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-04-04 08:28:50 -0300

    Implement Page.Events.Request tests (#114)
    * Implement Page.Events.Request tests
    
    * Skip Issue0100 test

--- lib/PuppeteerSharp.Tests/Page/UrlTests.cs
+++ lib/PuppeteerSharp.Tests/Page/UrlTests.cs
@@ -5,17 +5,14 @@ using Xunit;
 namespace PuppeteerSharp.Tests.Page
 {
     [Collection("PuppeteerLoaderFixture collection")]
-    public class UrlTests : PuppeteerBaseTest
+    public class UrlTests : PuppeteerPageBaseTest
     {
         [Fact]
         public async Task ShouldWork()
         {
-            using (var page = await Browser.NewPageAsync())
-            {
-                Assert.Equal(TestConstants.AboutBlank, page.Url);
-                await page.GoToAsync(TestConstants.EmptyPage);
-                Assert.Equal(TestConstants.EmptyPage, page.Url);
-            }
+            Assert.Equal(TestConstants.AboutBlank, Page.Url);
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            Assert.Equal(TestConstants.EmptyPage, Page.Url);
         }
     }
 }
