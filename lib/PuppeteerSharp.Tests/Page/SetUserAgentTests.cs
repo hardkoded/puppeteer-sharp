4e7545bb4e9d208db81462ad8bc1635fe16d84c8
Meir Blachman <meirblachman@gmail.com>
2018-04-08 18:58:33 +0300

    added tests

---
+++ lib/PuppeteerSharp.Tests/Page/SetUserAgentTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.Page
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class SetUserAgentTests : PuppeteerPageBaseTest
+    {
+        [Fact]
+        public async Task ShouldWork()
+        {
+            Assert.Contains("Mozilla", await Page.EvaluateFunctionAsync<string>("() => navigator.userAgent"));
+            await Page.SetUserAgentAsync("foobar");
+
+            // todo: make like puppeteer
+            Assert.Equal("foobar", await Page.EvaluateFunctionAsync<string>("() => navigator.userAgent"));
+        }
+    }
+}
