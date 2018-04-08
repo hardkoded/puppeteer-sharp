9597f4d6b0e69bd02a86d8db05b46e257e3e0c54
Meir Blachman <meirblachman@gmail.com>
2018-04-08 12:43:25 +0300

    auth tests

--- lib/PuppeteerSharp.Tests/TestConstants.cs
+++ lib/PuppeteerSharp.Tests/TestConstants.cs
@@ -13,7 +13,7 @@ namespace PuppeteerSharp.Tests
         public const string AboutBlank = "about:blank";
         public static readonly string CrossProcessHttpPrefix = "http://127.0.0.1:8907";
         public static readonly string EmptyPage = $"{ServerUrl}/empty.html";
-        public static readonly string CrossProcessAuthenticateUrl = $"{ServerIpUrl}/authenticationtest/testuser";
+        public static readonly string CrossProcessUrl = ServerIpUrl;
         public static readonly string AuthenticateUrl = $"{ServerUrl}/authenticationtest/testuser";
 
         public static LaunchOptions DefaultBrowserOptions() => new LaunchOptions
