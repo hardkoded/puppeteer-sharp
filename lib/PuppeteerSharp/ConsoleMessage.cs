022a361f73bc5c5b246519f457446bf5d73d8dda
phenx <phenxdesign@gmail.com>
2018-04-12 23:17:13 +0200

    Use native deserialization mapping for ConsoleType and DialogType

--- lib/PuppeteerSharp/ConsoleMessage.cs
+++ lib/PuppeteerSharp/ConsoleMessage.cs
@@ -6,35 +6,13 @@ namespace PuppeteerSharp
     {
         public ConsoleType Type { get; }
         public IList<JSHandle> Args { get; }
-        
-        private static readonly Dictionary<string, ConsoleType> ConsoleTypeMap = new Dictionary<string, ConsoleType>
-        {
-            ["log"] = ConsoleType.Log,
-            ["debug"] = ConsoleType.Debug,
-            ["trace"] = ConsoleType.Trace,
-            ["dir"] = ConsoleType.Dir,
-            ["warning"] = ConsoleType.Warning,
-            ["error"] = ConsoleType.Error,
-            ["time"] = ConsoleType.Time,
-            ["timeEnd"] = ConsoleType.TimeEnd,
-        };
 
-        public ConsoleMessage(string type, IList<JSHandle> args)
+        public ConsoleMessage(ConsoleType type, IList<JSHandle> args)
         {
-            Type = GetConsoleType(type);
+            Type = type;
             Args = args;
         }
 
         public string Text() => string.Join(" ", Args);
-
-        private static ConsoleType GetConsoleType(string consoleType)
-        {
-            if (ConsoleTypeMap.ContainsKey(consoleType))
-            {
-                return ConsoleTypeMap[consoleType];
-            }
-
-            throw new PuppeteerException($"Unknown javascript console type {consoleType}");
-        }
     }
 }
--- lib/PuppeteerSharp/Messaging/PageConsoleResponse.cs
+++ lib/PuppeteerSharp/Messaging/PageConsoleResponse.cs
@@ -5,7 +5,7 @@ namespace PuppeteerSharp.Messaging
     public class PageConsoleResponse
     {
         [JsonProperty("type")]
-        public string Type { get; set; }
+        public ConsoleType Type { get; set; }
         
         [JsonProperty("args")]
         public dynamic[] Args { get; set; }
