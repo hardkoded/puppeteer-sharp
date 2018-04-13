3c2332c8c17b2ef7f0b17bc9edc11211681806d6
Meir Blachman <meirblachman@gmail.com>
2018-04-13 08:01:54 +0300

    implemented Page.emulateMedia tests

---
+++ lib/PuppeteerSharp/MediaType.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace PuppeteerSharp
+{
+    [JsonConverter(typeof(StringEnumConverter), true)]
+    public enum MediaType
+    {
+        Print,
+
+        Screen,
+
+        [EnumMember(Value = "")]
+        None
+    }
+}
