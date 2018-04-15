9bb3064e14c46b85eeb99b1b7a4d1bc92194f7a9
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-04-15 09:52:16 -0300

    ups

--- lib/PuppeteerSharp.Tests/Page/Events/ErrorTests.cs
+++ lib/PuppeteerSharp.Tests/Page/Events/ErrorTests.cs
@@ -9,7 +9,7 @@ namespace PuppeteerSharp.Tests.Page.Events
     [Collection("PuppeteerLoaderFixture collection")]
     public class ErrorTests : PuppeteerPageBaseTest
     {
-        [Fact(Skip ="Is this test the problem?"]
+        [Fact(Skip ="Is this test the problem?")]
         public async Task ShouldThrowWhenPageCrashes()
         {
             string error = null;
