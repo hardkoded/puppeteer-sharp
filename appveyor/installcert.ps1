e6b767a2d87c8225ba332f8fa73b370f3ad1ec5a
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-04-17 19:11:18 -0300

    How to make happy a CI - Part XII

--- appveyor/installcert.ps1
+++ appveyor/installcert.ps1
@@ -1,3 +1,7 @@
+$cert = New-SelfSignedCertificate -DnsName ("localhost","127.0.0.1") -FriendlyName "Puppeteer" -CertStoreLocation cert:\LocalMachine\My
+$rootStore = Get-Item cert:\LocalMachine\Root
+$rootStore.Open("ReadWrite")
+$rootStore.Add($cert)
+$rootStore.Close();
 Import-Module WebAdministration
-Set-Location -Path cert:\LocalMachine\My
-Import-Certificate -Filepath "C:\projects\puppeteer-sharp\lib\PuppeteerSharp.TestServer\testCert.cer"
+Get-ChildItem -Path cert:\LocalMachine\my | where { $_.friendlyname -eq "Puppeteer" } | Export-Certificate -FilePath C:\projects\puppeteer-sharp\lib\PuppeteerSharp.TestServer\testCert.cer
