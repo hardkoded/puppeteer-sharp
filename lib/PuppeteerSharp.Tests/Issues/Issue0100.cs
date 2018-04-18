8f1d46308ab09dd9bd2039b8c74578172a67e7a0
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-04-18 17:30:24 -0300

    That page is taking too long, I'm taking that out

--- lib/PuppeteerSharp.Tests/Issues/Issue0100.cs
+++ lib/PuppeteerSharp.Tests/Issues/Issue0100.cs
@@ -8,17 +8,6 @@ namespace PuppeteerSharp.Tests.Issues
     public class Issue0100 : PuppeteerBaseTest
     {
         [Fact]
-        public async Task PdfWundergroundShouldWork()
-        {
-            using (var page = await Browser.NewPageAsync())
-            {
-                var response = await page.GoToAsync("https://www.wunderground.com/weather/be/antwerp");
-                var pdf = await page.PdfStreamAsync();
-                Assert.NotNull(pdf);
-            }
-        }
-
-        [Fact]
         public async Task PdfDarkskyShouldWork()
         {
             using (var page = await Browser.NewPageAsync())
