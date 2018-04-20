622b1f164d0b7ab5ea5b6192b4e2a78b0be75cad
Meir Blachman <meirblachman@gmail.com>
2018-04-20 10:40:42 +0300

    docs

--- lib/PuppeteerSharp/Coverage.cs
+++ lib/PuppeteerSharp/Coverage.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace PuppeteerSharp
 {
+    /// <summary>
+    /// gathers information about parts of JavaScript and CSS that were used by the page.
+    /// </summary>
     public class Coverage
     {
         private readonly JSCoverage _jsCoverage;
@@ -13,9 +16,21 @@ namespace PuppeteerSharp
             _jsCoverage = new JSCoverage(client);
         }
 
+        /// <summary>
+        /// Starts JS coverage
+        /// </summary>
+        /// <param name="options">Set of configurable options for coverage</param>
+        /// <returns>A task that resolves when coverage is started</returns>
         public Task StartJSCoverageAsync(JSCoverageStartOptions options = null)
             => _jsCoverage.StartAsync(options ?? new JSCoverageStartOptions());
 
+        /// <summary>
+        /// Stops JS coverage and returns coverage reports for all non-anonymous scripts
+        /// </summary>
+        /// <returns>Task that resolves to the array of coverage reports for all stylesheets</returns>
+        /// <remarks>
+        /// JavaScript Coverage doesn't include anonymous scripts; however, scripts with sourceURLs are reported.
+        /// </remarks>
         public Task StopJSCoverageAsync() => _jsCoverage.StopAsync();        
     }
 }
--- lib/PuppeteerSharp/JSCoverageStartOptions.cs
+++ lib/PuppeteerSharp/JSCoverageStartOptions.cs
@@ -1,7 +1,13 @@
 ﻿namespace PuppeteerSharp
 {
+    /// <summary>
+    /// Set of configurable options for coverage
+    /// </summary>
     public class JSCoverageStartOptions
     {
+        /// <summary>
+        /// Whether to reset coverage on every navigation. Defaults to <c>true</c>.
+        /// </summary>
         public bool ResetOnNavigation { get; set; } = true;
     }
 }
