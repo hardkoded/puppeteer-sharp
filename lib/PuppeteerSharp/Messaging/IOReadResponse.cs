3c00dfdfb7d390bd0a492eacf04adc20531d64d9
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-04-26 09:20:18 -0300

    Implement Tracing (#170)

---
+++ lib/PuppeteerSharp/Messaging/IOReadResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PuppeteerSharp.Messaging
+{
+    public class IOReadResponse
+    {
+        [JsonProperty("eof")]
+        public bool Eof { get; set; }
+        [JsonProperty("data")]
+        public string Data { get; set; }
+    }
+}
---
+++ lib/PuppeteerSharp/TracingOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace PuppeteerSharp
+{
+    public class TracingOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether Tracing should captures screenshots in the trace
+        /// </summary>
+        /// <value>Screenshots option</value>
+        public bool Screenshots { get; set; }
+        /// <summary>
+        /// A path to write the trace file to
+        /// </summary>
+        /// <value>The path.</value>
+        public string Path { get; set; }
+        /// <summary>
+        /// Specify custom categories to use instead of default.
+        /// </summary>
+        /// <value>The categories.</value>
+        public List<string> Categories { get; set; }
+    }
+}
