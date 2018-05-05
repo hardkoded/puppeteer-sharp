233def113b8a60d7933348486cef71056058dab6
Meir Blachman <meirblachman@gmail.com>
2018-05-05 20:56:45 +0300

    Implemented Page.$ tests

---
+++ lib/PuppeteerSharp.Tests/Page/GetElementTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.Page
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class GetElementTests : PuppeteerPageBaseTest
+    {
+        [Fact]
+        public async Task ShouldQueryExistingElement()
+        {
+            await Page.SetContentAsync("<section>test</section>");
+            var element = await Page.GetElementAsync("section");
+            Assert.NotNull(element);
+        }
+
+        [Fact]
+        public async Task ShouldReturnNullForNonExistingElement()
+        {
+            var element = await Page.GetElementAsync("non-existing-element");
+            Assert.Null(element);
+        }
+    }
+}
