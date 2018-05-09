24b4b4a5445085acfb0acdbc1b3538bb9f43177e
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-05-09 09:05:01 -0300

    Remove class

--- lib/PuppeteerSharp.Tests/Input/InputTests.cs
+++ lib/PuppeteerSharp.Tests/Input/InputTests.cs
@@ -540,16 +540,4 @@ namespace PuppeteerSharp.Tests.Input
             await Assert.ThrowsAsync<KeyNotFoundException>(() => Page.Keyboard.PressAsync("😊"));
         }
     }
-
-    class Dimensions
-    {
-        [JsonProperty("x")]
-        public decimal X { get; set; }
-        [JsonProperty("y")]
-        public decimal Y { get; set; }
-        [JsonProperty("width")]
-        public decimal Width { get; set; }
-        [JsonProperty("height")]
-        public decimal Height { get; set; }
-    }
 }
