7022ca1adc149a14648983a37eae6a744eb6e8db
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-05-09 18:57:19 -0300

    Implement ElementHandle.HoverAsync (#207)

---
+++ lib/PuppeteerSharp.Tests/ElementHandleTests/HoverTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PuppeteerSharp.Tests.ElementHandleTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class HoverTests : PuppeteerPageBaseTest
+    {
+        [Fact]
+        public async Task ShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.ServerUrl + "/input/scrollable.html");
+            var button = await Page.QuerySelectorAsync("#button-6");
+            await button.HoverAsync();
+            Assert.Equal("button-6", await Page.EvaluateExpressionAsync<string>(
+                "document.querySelector('button:hover').id"));
+        }
+    }
+}
