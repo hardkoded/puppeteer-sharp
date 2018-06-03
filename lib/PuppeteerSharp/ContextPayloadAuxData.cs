00836a94484e5a8ae4c5fb98448718deff641969
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-06-03 17:42:17 -0300

    Set JSON Properties

--- lib/PuppeteerSharp/ContextPayloadAuxData.cs
+++ lib/PuppeteerSharp/ContextPayloadAuxData.cs
@@ -1,8 +1,12 @@
+using Newtonsoft.Json;
+
 namespace PuppeteerSharp
 {
     internal struct ContextPayloadAuxData
     {
+        [JsonProperty("frameId")]
         internal string FrameId { get; set; }
+        [JsonProperty("isDefault")]
         internal bool IsDefault { get; set; }
     }
 }
