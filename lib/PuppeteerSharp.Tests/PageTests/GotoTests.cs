d4686ae80e1b97e74cb05fa8c8fb07d71d794570
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-14 13:52:29 -0300

    Remove extra declaration

--- lib/PuppeteerSharp.Tests/PageTests/GotoTests.cs
+++ lib/PuppeteerSharp.Tests/PageTests/GotoTests.cs
@@ -25,7 +25,7 @@ namespace PuppeteerSharp.Tests.PageTests
         [Fact]
         public async Task ShouldNavigateToEmptyPageWithDOMContentLoaded()
         {
-            var response = await Page.GoToAsync(TestConstants.EmptyPage, waitUntil: new WaitUntilNavigation[] {
+            var response = await Page.GoToAsync(TestConstants.EmptyPage, waitUntil: new[] {
                 WaitUntilNavigation.DOMContentLoaded
             });
             Assert.Equal(HttpStatusCode.OK, response.Status);
