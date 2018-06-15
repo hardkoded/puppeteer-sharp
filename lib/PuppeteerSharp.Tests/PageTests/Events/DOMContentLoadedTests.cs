b8629cc89baa8f0a8ac57fd32e6fb8734f3e2e03
Meir Blachman <meirblachman@gmail.com>
2018-06-15 11:18:43 +0300

    Implemented Page.Events.DOMContentLoaded

---
+++ lib/PuppeteerSharp.Tests/PageTests/Events/DOMContentLoadedTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.PageTests.Events
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class DOMContentLoadedTests : PuppeteerPageBaseTest
+    {
+        public DOMContentLoadedTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldFireWhenExpected()
+        {
+            var _ = Page.GoToAsync(TestConstants.AboutBlank);
+            var completion = new TaskCompletionSource<bool>();
+            Page.DOMContentLoaded += (sender, e) => completion.SetResult(true);
+            await completion.Task;
+        }
+    }
+}
