ff0984b18eaf0a6f8911a2f56a0a42d3ca39b756
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-06-18 16:13:44 -0300

    Implement Frame.EvaluateExpressionHandleAsync (#330)

---
+++ lib/PuppeteerSharp.Tests/FrameTests/EvaluateHandleTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.FrameTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class EvaluateHandleTests : PuppeteerPageBaseTest
+    {
+        public EvaluateHandleTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            var windowHandle = await Page.MainFrame.EvaluateExpressionHandleAsync("window");
+            Assert.NotNull(windowHandle);
+        }
+    }
+}
