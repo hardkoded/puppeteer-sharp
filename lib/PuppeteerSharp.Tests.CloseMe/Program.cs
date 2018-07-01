7be258cc928b30758edf036c9d8c18cff0b7d885
Dario Kondratiuk <dariokondratiuk@gmail.com>
2018-07-01 15:01:03 -0300

    Project

---
+++ lib/PuppeteerSharp.Tests.CloseMe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppeteerSharp.Tests.CloseMe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
