b034722c6ba7ab8cee53e6298c8fd8536e1df347
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-07-15 16:33:46 -0300

    Ups

--- lib/PuppeteerSharp/WaitTaskTimeoutException.cs
+++ lib/PuppeteerSharp/WaitTaskTimeoutException.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace PuppeteerSharp
 {
     [Serializable]
-    internal class WaitTaskTimeoutException : Exception
+    internal class WaitTaskTimeoutException : PuppeteerException
     {
         /// <summary>
         /// Timeout that caused the exception
