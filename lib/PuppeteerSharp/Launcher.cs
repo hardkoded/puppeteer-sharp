a4baa1c873a0d841973d07fe69513bee9d360617
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-07-16 13:11:08 -0300

    launch chrome with --disable-dev-shm-usage by default (#426)

--- lib/PuppeteerSharp/Launcher.cs
+++ lib/PuppeteerSharp/Launcher.cs
@@ -23,6 +23,7 @@ namespace PuppeteerSharp
             "--disable-background-timer-throttling",
             "--disable-client-side-phishing-detection",
             "--disable-default-apps",
+            "--disable-dev-shm-usage",
             "--disable-extensions",
             "--disable-hang-monitor",
             "--disable-popup-blocking",
