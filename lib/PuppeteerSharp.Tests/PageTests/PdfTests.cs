33bfa43c2ceaffb7b88676ba5b760976984f6f44
Meir Blachman <meirblachman@gmail.com>
2018-07-28 20:37:20 +0300

    cr

---
+++ lib/PuppeteerSharp.Tests/PageTests/PdfTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.PageTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class PdfTests : PuppeteerPageBaseTest
+    {
+        public PdfTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToSaveFile()
+        {
+            var outputFile = Path.Combine(BaseDirectory, "output.pdf");
+            var fileInfo = new FileInfo(outputFile);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+            await Page.PdfAsync(outputFile);
+            fileInfo = new FileInfo(outputFile);
+            Assert.True(new FileInfo(outputFile).Length > 0);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+        }
+    }
+}
