11f001509a08fe6572bb25848cdc57427a000743
Meir Blachman <meirblachman@gmail.com>
2018-08-10 11:14:05 +0300

    added elementHandle.$$eval method

--- lib/PuppeteerSharp.Tests/ElementHandleTests/EvaluateFunctionTests.cs
+++ lib/PuppeteerSharp.Tests/ElementHandleTests/EvaluateFunctionTests.cs
@@ -12,7 +12,7 @@ namespace PuppeteerSharp.Tests.ElementHandleTests
         }
 
         [Fact]
-        public async Task ShouldWork()
+        public async Task QuerySelectorShouldWork()
         {
             await Page.SetContentAsync("<html><body><div class='tweet'><div class='like'>100</div><div class='retweets'>10</div></div></body></html>");
             var tweet = await Page.QuerySelectorAsync(".tweet");
@@ -22,7 +22,7 @@ namespace PuppeteerSharp.Tests.ElementHandleTests
         }
 
         [Fact]
-        public async Task ShouldRetrieveContentFromSubtree()
+        public async Task QuerySelectorShouldRetrieveContentFromSubtree()
         {
             var htmlContent = "<div class='a'>not-a-child-div</div><div id='myId'><div class='a'>a-child-div</div></div>";
             await Page.SetContentAsync(htmlContent);
@@ -33,7 +33,7 @@ namespace PuppeteerSharp.Tests.ElementHandleTests
         }
 
         [Fact]
-        public async Task ShouldThrowInCaseOfMissingSelector()
+        public async Task QuerySelectorShouldThrowInCaseOfMissingSelector()
         {
             var htmlContent = "<div class=\"a\">not-a-child-div</div><div id=\"myId\"></div>";
             await Page.SetContentAsync(htmlContent);
@@ -42,6 +42,39 @@ namespace PuppeteerSharp.Tests.ElementHandleTests
                 () => elementHandle.QuerySelectorAsync(".a").EvaluateFunctionAsync<string>("node => node.innerText")
             );
             Assert.Equal("Error: failed to find element matching selector", exception.Message);
+        }
+
+        [Fact]
+        public async Task QuerySelectorAllShouldWork()
+        {
+            await Page.SetContentAsync("<html><body><div class='tweet'><div class='like'>100</div><div class='like'>10</div></div></body></html>");
+            var tweet = await Page.QuerySelectorAsync(".tweet");
+            var content = await tweet.QuerySelectorAllHandleAsync(".like")
+                .EvaluateFunctionAsync<string[]>("nodes => nodes.map(n => n.innerText)");
+            Assert.Equal(new[] { "100", "10" }, content);
+        }
+
+        [Fact]
+        public async Task QuerySelectorAllShouldRetrieveContentFromSubtree()
+        {
+            var htmlContent = "<div class='a'>not-a-child-div</div><div id='myId'><div class='a'>a1-child-div</div><div class='a'>a2-child-div</div></div>";
+            await Page.SetContentAsync(htmlContent);
+            var elementHandle = await Page.QuerySelectorAsync("#myId");
+            var content = await elementHandle.QuerySelectorAllHandleAsync(".a")
+                .EvaluateFunctionAsync<string[]>("nodes => nodes.map(n => n.innerText)");
+            Assert.Equal(new[] { "a1-child-div", "a2-child-div" }, content);
+        }
+
+        [Fact]
+        public async Task QuerySelectorAllShouldThrowInCaseOfMissingSelector()
+        {
+            var htmlContent = "<div class='a'>not-a-child-div</div><div id='myId'></div>";
+            await Page.SetContentAsync(htmlContent);
+            var elementHandle = await Page.QuerySelectorAsync("#myId");
+            var exception = await Assert.ThrowsAsync<SelectorException>(
+                () => elementHandle.QuerySelectorAllHandleAsync(".a")
+                .EvaluateFunctionAsync<object>("nodes => nodes.map(n => n.innerText)"));
+            Assert.Equal("Error: failed to find elements matching selector", exception.Message);
         }
     }
 }
