972beb5b65906108ed890329387ccce30bd0a9bf
Gerke Geurts <ggeurts@geurtsrus.com>
2018-08-27 15:18:03 +0200

    Rewrite of TestUtils.CompressText

--- lib/PuppeteerSharp.Tests/TestUtils.cs
+++ lib/PuppeteerSharp.Tests/TestUtils.cs
@@ -16,11 +16,44 @@ namespace PuppeteerSharp.Tests
             return Path.Combine(current, directory);
         }
 
-        public static string CompressText(string text) => new StringBuilder(text)
-            .Replace("\r", "")
-            .Replace("\n", "")
-            .Replace("\t", "")
-            .Replace(" ", "")
-            .ToString();
+        /// <summary>
+        /// Removes as much whitespace as possible from a given string. Whitespace
+        /// that separates letters and/or digits is collapsed to a space character.
+        /// Other whitespace is fully removed.
+        /// </summary>
+        public static string CompressText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            var sb = new StringBuilder();
+            var inWhitespace = false;
+            foreach (var ch in text)
+            {
+                if (char.IsWhiteSpace(ch))
+                {
+                    if (ch != '\n' && ch != '\r')
+                    {
+                        inWhitespace = true;
+                    }
+                }
+                else
+                {
+                    if (inWhitespace)
+                    {
+                        inWhitespace = false;
+                        if (sb.Length > 0 && char.IsLetterOrDigit(sb[sb.Length - 1]) && char.IsLetterOrDigit(ch))
+                        {
+                            sb.Append(' ');
+                        }
+                    }
+                    sb.Append(ch);
+                }
+            }
+
+            return sb.ToString();
+        }
     }
 }
