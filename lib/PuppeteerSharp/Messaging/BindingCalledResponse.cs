2b0fa15fa41ec735da7adcc155c06f277bf7b645
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-06 08:37:24 -0300

    Migrate exposeFunction from console.debug to Runtime.installBinding

---
+++ lib/PuppeteerSharp/Messaging/BindingCalledResponse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PuppeteerSharp.Messaging
+{
+    internal class BindingCalledResponse
+    {
+        private string _payloadJson;
+        [JsonProperty("payload")]
+        public string PayloadJson
+        {
+            get => _payloadJson;
+            set
+            {
+                _payloadJson = value;
+                var json = JsonConvert.DeserializeObject(_payloadJson) as JObject;
+                Payload = json.ToObject<BindingPayload>();
+                Payload.JsonObject = json;
+
+            }
+        }
+
+        public BindingPayload Payload { get; set; }
+
+        [JsonProperty("executionContextId")]
+        public object ExecutionContextId { get; set; }
+
+        public class BindingPayload
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("args")]
+            public object[] Args { get; set; }
+            [JsonProperty("seq")]
+            public int Seq { get; set; }
+
+            public JObject JsonObject { get; set; }
+        }
+    }
+}
