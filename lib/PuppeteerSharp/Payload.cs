f1037daace7fc88bee1b673c01d4918bb9c2ec92
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-10 08:23:45 -0300

    Remove header 'cookie' from request hash

--- lib/PuppeteerSharp/Payload.cs
+++ lib/PuppeteerSharp/Payload.cs
@@ -71,9 +71,10 @@ namespace PuppeteerSharp
                 {
                     foreach (var item in Headers.OrderBy(kv => kv.Key))
                     {
-                        if (item.Key.Equals("Accept", StringComparison.OrdinalIgnoreCase)
-                            || item.Key.Equals("Referer", StringComparison.OrdinalIgnoreCase)
-                            || item.Key.Equals("X-DevTools-Emulate-Network-Conditions-Client-Id", StringComparison.OrdinalIgnoreCase))
+                        if (item.Key.Equals("accept", StringComparison.OrdinalIgnoreCase)
+                            || item.Key.Equals("referer", StringComparison.OrdinalIgnoreCase)
+                            || item.Key.Equals("x-devtools-emulate-network-conditions-client-id", StringComparison.OrdinalIgnoreCase)
+                            || item.Key.Equals("cookie", StringComparison.OrdinalIgnoreCase))
                         {
                             continue;
                         }
