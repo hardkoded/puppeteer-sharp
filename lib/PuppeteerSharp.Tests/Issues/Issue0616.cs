9e4eb2bf0139bca80d844e377b49c0d22d968199
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-14 08:04:46 -0300

    Improve Request overrides on ContinueAsync

---
+++ lib/PuppeteerSharp.Tests/Issues/Issue0616.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.PageTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class Issue0616 : PuppeteerPageBaseTest
+    {
+        public Issue0616(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToChangeToPost()
+        {
+            await Page.SetRequestInterceptionAsync(true);
+            Page.Request += async (sender, e) =>
+            {
+                var payload = new Payload()
+                {
+                    Method = HttpMethod.Post,
+                    PostData = "foo"
+                };
+                await e.Request.ContinueAsync(payload);
+            };
+
+            var requestTask = Server.WaitForRequest<string>("/grid.html", request =>
+            {
+                return request.Form.ToString();
+            });
+            var gotoTask = Page.GoToAsync(TestConstants.ServerUrl + "/grid.html");
+
+            await Task.WhenAll(
+                requestTask,
+                gotoTask
+            );
+
+            Assert.Equal("http://google.com/", requestTask.Result);
+        }
+    }
+}
