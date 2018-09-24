ddb4109ccf6097c7b0d8c082feb19f6dd42004bf
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-24 18:22:35 -0300

    Add missing property

---
+++ lib/PuppeteerSharp/Messaging/NetworkGetResponseBodyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PuppeteerSharp.Messaging
+{
+    internal class NetworkGetResponseBodyResponse
+    {
+        [JsonProperty("body")]
+        public string Body { get; set; }
+        [JsonProperty("base64Encoded")]
+        public bool Base64Encoded { get; set; }
+    }
+}
