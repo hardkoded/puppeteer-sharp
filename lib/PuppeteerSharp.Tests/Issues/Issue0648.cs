e275735bda26c31389f8e5ba181a5abd0e9a51c2
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-26 08:42:23 -0300

    New ResourceType.Ping

---
+++ lib/PuppeteerSharp.Tests/Issues/Issue0648.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.Issues
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class Issue0648 : PuppeteerPageBaseTest
+    {
+        public Issue0648(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldWork()
+        {
+            await Page.SetRequestInterceptionAsync(true);
+            Page.Request += async (sender, e) => await e.Request.ContinueAsync();
+            await Page.GoToAsync("https://www.google.com/search?q=firewall&oq=firewall&ie=UTF-8");
+        }
+    }
+}
