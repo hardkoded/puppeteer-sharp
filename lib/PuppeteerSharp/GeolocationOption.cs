017e91e2a84526d8f36d84221b8727f0e8f984d4
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-09-30 18:24:13 -0300

    Geolocation options

---
+++ lib/PuppeteerSharp/GeolocationOption.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace PuppeteerSharp
+{
+    /// <summary>
+    /// Geolocation option.
+    /// </summary>
+    /// <seealso cref="Page.SetGeolocationAsync(GeolocationOption)"/>
+    public class GeolocationOption
+    {
+        /// <summary>
+        /// Latitude between -90 and 90.
+        /// </summary>
+        /// <value>The latitude.</value>
+        public decimal Latitude { get; set; }
+        /// <summary>
+        /// Longitude between -180 and 180.
+        /// </summary>
+        /// <value>The longitude.</value>
+        public decimal Longitude { get; set; }
+        /// <summary>
+        /// Optional non-negative accuracy value.
+        /// </summary>
+        /// <value>The accuracy.</value>
+        public decimal Accuracy { get; set; }
+    }
+}
