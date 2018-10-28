19ad79ebd31e5add73c6f3e36419e8df325d46a7
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-10-28 08:50:02 -0300

    Implement ConnectionTransport (#710)

---
+++ lib/PuppeteerSharp/Transport/MessageReceivedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PuppeteerSharp.Transport
+{
+    /// <summary>
+    /// Message received event arguments.
+    /// <see cref="IConnectionTransport.MessageReceived"/>
+    /// </summary>
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PuppeteerSharp.Transport.MessageReceivedEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public MessageReceivedEventArgs(string message) => Message = message;
+        /// <summary>
+        /// Transport message.
+        /// </summary>
+        public string Message { get; }
+    }
+}
