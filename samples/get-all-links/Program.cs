810f1c2f9758456da3b3ffea11ed844445227722
Bilal Durrani <bilal33@gmail.com>
2018-10-30 20:47:28 -0400

    getting started with example

---
+++ samples/get-all-links/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace get_all_links
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
