7de790d0a14f6110552dc8890d9895b773163cad
Bilal Durrani <bilal33@gmail.com>
2018-11-04 20:51:14 -0500

    starting example

---
+++ samples/complex-js-objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace complex_js_objects
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new LaunchOptions { Headless = true };
+            Console.WriteLine("Downloading chromium");
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            Console.WriteLine("Navigating to google.com");
+            using (var browser = await Puppeteer.LaunchAsync(options))
+            using (var page = await browser.NewPageAsync())
+            {
+                await page.GoToAsync("http://www.google.com");
+                var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
+                var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
+                foreach (string url in urls)
+                {
+                    Console.WriteLine($"Url: {url}");
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
