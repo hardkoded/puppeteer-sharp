667dacb7c9dfdd137bcf57f6cb72a14659b4d650
Bilal Durrani <bilal33@gmail.com>
2018-11-05 23:02:37 -0500

    data as custom POCO

--- samples/complex-js-objects/Program.cs
+++ samples/complex-js-objects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using PuppeteerSharp;
 
@@ -6,22 +7,34 @@ namespace complex_js_objects
 {
     class Program
     {
+        [DebuggerDisplay("Content: {Content} Url: {Url}")]
+        public class Data
+        {
+            public string Content { get; set; }
+            public string Url { get; set; }
+        }
+
         static async Task Main(string[] args)
         {
             var options = new LaunchOptions { Headless = true };
             Console.WriteLine("Downloading chromium");
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-            Console.WriteLine("Navigating to google.com");
+            Console.WriteLine("Navigating to Hacker News");
             using (var browser = await Puppeteer.LaunchAsync(options))
             using (var page = await browser.NewPageAsync())
             {
-                await page.GoToAsync("http://www.google.com");
-                var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
-                var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
-                foreach (string url in urls)
-                {
-                    Console.WriteLine($"Url: {url}");
-                }
+                await page.GoToAsync("https://news.ycombinator.com/");
+                Console.WriteLine("Get all urls from page");
+                var jsCode = @"
+const selectors = Array.from(document.querySelectorAll('a[class=""storylink""]')); 
+selectors.map( t=> {return {content: t.innerHTML, url: t.href}});
+";
+                var handles = await page.EvaluateExpressionHandleAsync(jsCode);
+                var results = await handles.JsonValueAsync<Data[]>();
+                //foreach (string url in urls)
+                //{
+                //    Console.WriteLine($"Url: {url}");
+                //}
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadLine();
             }
