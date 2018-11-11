5dcd832d3b455f8b834f0932779fdc56152028b5
Bilal Durrani <bilal33@gmail.com>
2018-11-10 23:09:43 -0500

    finish the example

--- samples/searching/Program.cs
+++ samples/searching/Program.cs
@@ -8,7 +8,7 @@ namespace Example.Searching
     {
         public static async Task Main(string[] args)
         {
-            var options = new LaunchOptions { Headless = false };
+            var options = new LaunchOptions { Headless = true };
             Console.WriteLine("Downloading chromium");
 
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
@@ -20,6 +20,12 @@ namespace Example.Searching
                 await page.GoToAsync("https://developers.google.com/web/");
                 // Type into search box.
                 await page.TypeAsync("#searchbox input", "Headless Chrome");
+
+                // Wait for suggest overlay to appear and click "show all results".
+                var allResultsSelector = ".devsite-suggest-all-results";
+                await page.WaitForSelectorAsync(allResultsSelector);
+                await page.ClickAsync(allResultsSelector);
+
                 // Wait for the results page to load and display the results.
                 var resultsSelector = ".gsc-results .gsc-thumbnail-inside a.gs-title";
                 await page.WaitForSelectorAsync(resultsSelector);
@@ -30,6 +36,11 @@ namespace Example.Searching
       return `${title} - ${anchor.href}`;
     });
 }", resultsSelector);
+
+                foreach (var link in links)
+                {
+                    Console.WriteLine(link); 
+                }
 
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadLine();
--- samples/searching/README.md
+++ samples/searching/README.md
@@ -1,1 +1,4 @@
-﻿
+﻿# Scraping search results from a website
+
+This example searches developers.google.com/web for articles tagged
+"Headless Chrome" and scrape results from the results page.
