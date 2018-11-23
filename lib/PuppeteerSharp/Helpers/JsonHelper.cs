01b344688736698d25b8486fc5fc025e9b4c014a
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-11-23 09:38:12 -0300

    cr

--- lib/PuppeteerSharp/Helpers/JsonHelper.cs
+++ lib/PuppeteerSharp/Helpers/JsonHelper.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace PuppeteerSharp.Helpers
 {
     internal static class JsonHelper
     {
-        public static JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings
+        public static readonly JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver(),
             NullValueHandling = NullValueHandling.Ignore
