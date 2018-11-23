1285e579668f3442289ee49a28767f168cd33cbd
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-11-23 09:05:26 -0300

    Remove NullVallueHandling

--- lib/PuppeteerSharp/CookieParam.cs
+++ lib/PuppeteerSharp/CookieParam.cs
@@ -24,49 +24,41 @@ namespace PuppeteerSharp
         /// Gets or sets the domain.
         /// </summary>
         /// <value>The domain.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Domain { get; set; }
         /// <summary>
         /// Gets or sets the URL.
         /// </summary>
         /// <value>The URL.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Url { get; set; }
         /// <summary>
         /// Gets or sets the path.
         /// </summary>
         /// <value>The path.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Path { get; set; }
         /// <summary>
         /// Gets or sets the expiration.
         /// </summary>
         /// <value>Expiration.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public double? Expires { get; set; }
         /// <summary>
         /// Gets or sets the size.
         /// </summary>
         /// <value>The size.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? Size { get; set; }
         /// <summary>
         /// Gets or sets if it's HTTP only.
         /// </summary>
         /// <value>Whether it's http only or not.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public bool? HttpOnly { get; set; }
         /// <summary>
         /// Gets or sets if it's secure.
         /// </summary>
         /// <value>Whether it's secure or not.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public bool? Secure { get; set; }
         /// <summary>
         /// Gets or sets if it's session only.
         /// </summary>
         /// <value>Whether it's session only or not.</value>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public bool? Session { get; set; }
     }
 }
