9e11c6f5d3de0a0717ca7f070f5c4849f0c18659
Darío Kondratiuk <dariokondratiuk@gmail.com>
2018-12-22 19:00:59 -0300

    Change TextAsync output type (#812)
    * Playground
    
    * Change ValueTask to Task

--- lib/PuppeteerSharp.Tests/FrameTests/GoToTests.cs
+++ lib/PuppeteerSharp.Tests/FrameTests/GoToTests.cs
@@ -77,9 +77,8 @@ namespace PuppeteerSharp.Tests.FrameTests
             {
                 serverResponses[i].TrySetResult(serverResponseTexts[i]);
                 var response = await navigations[i];
-                var responseText = await response.TextAsync();
                 Assert.Same(frameTasks[i].Result, response.Frame);
-                Assert.Equal(serverResponseTexts[i], responseText);
+                Assert.Equal(serverResponseTexts[i], await response.TextAsync());
             }
         }
     }
