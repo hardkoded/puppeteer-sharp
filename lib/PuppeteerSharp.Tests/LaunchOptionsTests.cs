e19e23eeb3662a19331ff3daa7b9a8956e36c56d
Henk Mollema <henkmollema@gmail.com>
2019-01-02 17:18:17 +0100

    Set headless option to false when Devtools are enabled

---
+++ lib/PuppeteerSharp.Tests/LaunchOptionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests
+{
+    public class LaunchOptionsTests
+    {
+        [Fact]
+        public void DisableHeadlessWhenDevtoolsEnabled() 
+        {
+            var options = new LaunchOptions
+            {
+                Devtools = true
+            };
+
+            Assert.True(options.Devtools);
+            Assert.False(options.Headless);
+        }
+    }
+}
