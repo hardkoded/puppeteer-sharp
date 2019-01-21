f3dbee1e2fbc981d207bcef1d728d6606a6abbbe
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-01-21 19:41:03 -0300

    Ignore string values in Keyboard

--- lib/PuppeteerSharp/Messaging/InputDispatchKeyEventRequest.cs
+++ lib/PuppeteerSharp/Messaging/InputDispatchKeyEventRequest.cs
@@ -1,13 +1,19 @@
-﻿namespace PuppeteerSharp.Messaging
+﻿using Newtonsoft.Json;
+
+namespace PuppeteerSharp.Messaging
 {
     internal class InputDispatchKeyEventRequest
     {
         public DispatchKeyEventType Type { get; set; }
         public int Modifiers { get; set; }
         public int WindowsVirtualKeyCode { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Code { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Key { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Text { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string UnmodifiedText { get; set; }
         public bool AutoRepeat { get; set; }
         public int Location { get; set; }
