4310ef80f6536cf74e23549750f1150d8deb5042
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-01-21 19:05:25 -0300

    Add Converters

--- lib/PuppeteerSharp/Messaging/MouseEventType.cs
+++ lib/PuppeteerSharp/Messaging/MouseEventType.cs
@@ -1,7 +1,10 @@
 ﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace PuppeteerSharp.Messaging
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     internal enum MouseEventType
     {
         [EnumMember(Value = "mouseMoved")]
