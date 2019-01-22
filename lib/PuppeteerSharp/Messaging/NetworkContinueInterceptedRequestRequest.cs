f896d4ea348ec0e98da7fd856d3992474562f6ba
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-01-22 08:29:17 -0300

    Json ignores

--- lib/PuppeteerSharp/Messaging/NetworkContinueInterceptedRequestRequest.cs
+++ lib/PuppeteerSharp/Messaging/NetworkContinueInterceptedRequestRequest.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PuppeteerSharp.Messaging
 {
     internal class NetworkContinueInterceptedRequestRequest
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string InterceptionId { get; set; }
         public NetworkContinueInterceptedRequestChallengeResponse AuthChallengeResponse { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string RawResponse { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string ErrorReason { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Url { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Method { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string PostData { get; set; }
         public Dictionary<string, string> Headers { get; set; }
     }
 
     internal class NetworkContinueInterceptedRequestChallengeResponse
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Response { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Username { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Password { get; set; }
     }
 }
