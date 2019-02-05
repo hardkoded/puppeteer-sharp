3c44e2ac4f3f64c7996167ed45e3ddd8155ed665
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-02-05 08:12:24 -0300

    Flexible ResourceType parse

--- lib/PuppeteerSharp/ResourceType.cs
+++ lib/PuppeteerSharp/ResourceType.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using PuppeteerSharp.Helpers;
 using System.Runtime.Serialization;
 
 namespace PuppeteerSharp
@@ -8,9 +9,13 @@ namespace PuppeteerSharp
     /// Resource type.
     /// </summary>
     /// <seealso cref="Request.ResourceType"/>
-    [JsonConverter(typeof(StringEnumConverter), true)]
+    [JsonConverter(typeof(FlexibleStringEnumConverter), Unknown)]
     public enum ResourceType
     {
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        Unknown = -1,
         /// <summary>
         /// Document.
         /// </summary>
