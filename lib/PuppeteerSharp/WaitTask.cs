dc3b4efd30e7e38fb0cc11ef084194344a1b66c1
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-02-08 08:14:21 -0300

    Page.WaitForFunctionAsync should work with multiline strings

--- lib/PuppeteerSharp/WaitTask.cs
+++ lib/PuppeteerSharp/WaitTask.cs
@@ -130,7 +130,7 @@ async function waitForPredicatePageFunction(predicateBody, polling, timeout, ...
             }
 
             _frame = frame;
-            _predicateBody = isExpression ? $"return {predicateBody}" : $"return ( {predicateBody} )(...args)";
+            _predicateBody = isExpression ? $"return ({predicateBody})" : $"return ({predicateBody})(...args)";
             _polling = polling;
             _pollingInterval = pollingInterval;
             _timeout = timeout;
