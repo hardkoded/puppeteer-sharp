3bfaf8bec2865017abb1b28fe8c75a63b7279931
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-03-11 08:03:25 -0300

    RequestPostDataTests tests (#1000)

---
+++ lib/PuppeteerSharp.Tests/NetworkTests/RequestPostDataTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.NetworkTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class RequestPostDataTests : PuppeteerPageBaseTest
+    {
+        public RequestPostDataTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            Server.SetRoute("/post", context => Task.CompletedTask);
+            Request request = null;
+            Page.Request += (sender, e) => request = e.Request;
+            await Page.EvaluateExpressionHandleAsync("fetch('./post', { method: 'POST', body: JSON.stringify({ foo: 'bar'})})");
+            Assert.NotNull(request);
+            Assert.Equal("{\"foo\":\"bar\"}", request.PostData);
+        }
+
+        [Fact]
+        public async Task ShouldBeUndefinedWhenThereIsNoPostData()
+        {
+            var response = await Page.GoToAsync(TestConstants.EmptyPage);
+            Assert.Null(response.Request.PostData);
+        }
+    }
+}
