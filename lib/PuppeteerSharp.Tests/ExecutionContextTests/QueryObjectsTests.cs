66056c881d39cec713c2e1d99136293a9daac683
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-04-29 14:05:24 -0300

    Add a test for queryObjects with navigation (#1097)

--- lib/PuppeteerSharp.Tests/ExecutionContextTests/QueryObjectsTests.cs
+++ lib/PuppeteerSharp.Tests/ExecutionContextTests/QueryObjectsTests.cs
@@ -24,6 +24,18 @@ namespace PuppeteerSharp.Tests.ExecutionContextTests
             Assert.Equal(new[] { "hello", "world" }, values);
         }
 
+        [Fact(Skip = "See https://github.com/GoogleChrome/puppeteer/issues/4263")]
+        public async Task ShouldWorkForNonBlankPage()
+        {
+            // Instantiate an object
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            await Page.EvaluateFunctionAsync("() => window.set = new Set(['hello', 'world'])");
+            var prototypeHandle = await Page.EvaluateFunctionHandleAsync("() => Set.prototype");
+            var objectsHandle = await Page.QueryObjectsAsync(prototypeHandle);
+            var count = await Page.EvaluateFunctionAsync<int>("objects => objects.length", objectsHandle);
+            Assert.Equal(1, count);
+        }
+
         [Fact]
         public async Task ShouldFailForDisposedHandles()
         {
