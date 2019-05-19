d109124cee2b7e50c60bcb8f82bc53c30d457546
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-05-19 19:51:35 -0300

    Add Browser.IsConnected (#1131)
    * Add Browser.IsConnected
    
    * Remove await

---
+++ lib/PuppeteerSharp.Tests/BrowserTests/IsConnectedTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.BrowserTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class IsConnectedTests : PuppeteerBrowserBaseTest
+    {
+        public IsConnectedTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ShouldSetTheBrowserConnectedState()
+        {
+            var newBrowser = await Puppeteer.ConnectAsync(new ConnectOptions
+            {
+                BrowserWSEndpoint = Browser.WebSocketEndpoint
+            });
+            Assert.True(newBrowser.IsConnected);
+            newBrowser.Disconnect();
+            Assert.False(newBrowser.IsConnected);
+        }
+    }
+}
