6b9f06f2835b28fe53d8199900704e6b9a5e549f
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-06-26 08:57:56 -0300

    Use .NET HttpCode phrases (#1187)
    * Allow fulfilling requests with status code 422
    
    * Change to an available code

--- lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
+++ lib/PuppeteerSharp/Messaging/FetchFulfillRequest.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.WebUtilities;
+
 namespace PuppeteerSharp.Messaging
 {
     internal class FetchFulfillRequest
     {
         public string RequestId { get; set; }
         public int ResponseCode { get; set; }
+        public string ResponsePhrase => ReasonPhrases.GetReasonPhrase(ResponseCode);
         public Header[] ResponseHeaders { get; set; }
         public string Body { get; set; }
     }
