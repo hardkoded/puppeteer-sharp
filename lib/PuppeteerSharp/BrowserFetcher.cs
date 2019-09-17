c79493d8fbd90fda1e818a640ef860e39591cd3a
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-09-17 08:27:18 -0300

    Roll Chromium to r682225

--- lib/PuppeteerSharp/BrowserFetcher.cs
+++ lib/PuppeteerSharp/BrowserFetcher.cs
@@ -38,7 +38,7 @@ namespace PuppeteerSharp
         /// <summary>
         /// Default Chromium revision.
         /// </summary>
-        public const int DefaultRevision = 674921;
+        public const int DefaultRevision = 682225;
 
         /// <summary>
         /// Gets the downloads folder.
