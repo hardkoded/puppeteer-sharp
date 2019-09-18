9febc70d17288b4e5e2a24dc638c055bbb82a890
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-09-18 08:34:02 -0300

    Add default context cookies test

---
+++ lib/PuppeteerSharp.Tests/BrowserContextTests/DefaultBrowserContextTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.BrowserContextTests
+{
+    [Collection("PuppeteerLoaderFixture collection")]
+    public class DefaultBrowserContextTests : PuppeteerPageBaseTest
+    {
+        public DefaultBrowserContextTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task PageCookiesShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+
+            await Page.EvaluateExpressionAsync("document.cookie = 'username=John Doe'");
+            var cookie = (await Page.GetCookiesAsync()).FirstOrDefault();
+            Assert.Equal("username", cookie.Name);
+            Assert.Equal("John Doe", cookie.Value);
+            Assert.Equal("localhost", cookie.Domain);
+            Assert.Equal("/", cookie.Path);
+            Assert.Equal(cookie.Expires, -1);
+            Assert.Equal(16, cookie.Size);
+            Assert.False(cookie.HttpOnly);
+            Assert.False(cookie.Secure);
+            Assert.True(cookie.Session);
+        }
+
+        [Fact]
+        public async Task PageSetCookieShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+
+            await Page.SetCookieAsync(new CookieParam
+            {
+                Name = "username",
+                Value = "John Doe"
+            });
+
+            var cookie = (await Page.GetCookiesAsync()).FirstOrDefault();
+            Assert.Equal("username", cookie.Name);
+            Assert.Equal("John Doe", cookie.Value);
+            Assert.Equal("localhost", cookie.Domain);
+            Assert.Equal("/", cookie.Path);
+            Assert.Equal(cookie.Expires, -1);
+            Assert.Equal(16, cookie.Size);
+            Assert.False(cookie.HttpOnly);
+            Assert.False(cookie.Secure);
+            Assert.True(cookie.Session);
+        }
+
+        [Fact]
+        public async Task PageDeleteCookieShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+
+            await Page.SetCookieAsync(
+                new CookieParam
+                {
+                    Name = "cookie1",
+                    Value = "1"
+                },
+                new CookieParam
+                {
+                    Name = "cookie2",
+                    Value = "2"
+                });
+
+            Assert.Equal("cookie1=1; cookie2=2", await Page.EvaluateExpressionAsync<string>("document.cookie"));
+            await Page.DeleteCookieAsync(new CookieParam
+            {
+                Name = "cookie2",
+                Value = "2"
+            });
+            Assert.Equal("cookie1=1", await Page.EvaluateExpressionAsync<string>("document.cookie"));
+
+            var cookie = (await Page.GetCookiesAsync()).FirstOrDefault();
+            Assert.Equal("cookie1", cookie.Name);
+            Assert.Equal("1", cookie.Value);
+            Assert.Equal("localhost", cookie.Domain);
+            Assert.Equal("/", cookie.Path);
+            Assert.Equal(cookie.Expires, -1);
+            Assert.Equal(8, cookie.Size);
+            Assert.False(cookie.HttpOnly);
+            Assert.False(cookie.Secure);
+            Assert.True(cookie.Session);
+        }
+    }
+}
