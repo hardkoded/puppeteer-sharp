08c6dd168d8655f650d6c565c1dfe2ee534e6b37
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-06-18 00:28:17 -0300

    more

--- lib/PuppeteerSharp/FirefoxProcess.cs
+++ lib/PuppeteerSharp/FirefoxProcess.cs
@@ -17,9 +17,7 @@ namespace PuppeteerSharp
     {
         #region Instance fields
 
-        private readonly LaunchOptions _options;
         private readonly TempDirectory _tempUserDataDir;
-        private readonly ILogger _logger;
         private readonly TaskCompletionSource<string> _startCompletionSource = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
         private readonly TaskCompletionSource<bool> _exitCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
         private State _currentState = State.Initial;
