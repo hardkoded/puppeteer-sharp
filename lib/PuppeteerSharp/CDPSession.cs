1c02c9ffbef08549d87f81185340a5249531f764
Jeff Peirson <Jeff.Peirson@a2ius.com>
2020-09-11 08:41:26 -0400

    Reverted if branch change

--- lib/PuppeteerSharp/CDPSession.cs
+++ lib/PuppeteerSharp/CDPSession.cs
@@ -184,25 +184,22 @@ namespace PuppeteerSharp
         {
             var id = obj.Id;
 
-            if (id.HasValue)
+            if (id.HasValue && _callbacks.TryRemove(id.Value, out var callback))
             {
-                if (_callbacks.TryRemove(id.Value, out var callback))
+                // This is a response to a SendAsync. Handle the callback async, since (a) we can, and (b)
+                // to avoid a situation where the continuation tries to call SendAsync again thus creating
+                // a deadlock.
+                Task.Run(() =>
                 {
-                    // This is a response to a SendAsync. Handle the callback async, since (a) we can, and (b)
-                    // to avoid a situation where the continuation tries to call SendAsync again thus creating
-                    // a deadlock.
-                    Task.Run(() =>
+                    if (obj.Error != null)
                     {
-                        if (obj.Error != null)
-                        {
-                            callback.TaskWrapper.TrySetException(new MessageException(callback, obj.Error));
-                        }
-                        else
-                        {
-                            callback.TaskWrapper.TrySetResult(obj.Result);
-                        }
-                    });
-                }
+                        callback.TaskWrapper.TrySetException(new MessageException(callback, obj.Error));
+                    }
+                    else
+                    {
+                        callback.TaskWrapper.TrySetResult(obj.Result);
+                    }
+                });
             }
             else
             {
