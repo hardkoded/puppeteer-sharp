9b16fd61e3c45591beedbf4d4a5c6e29da6c9907
Mateusz Redynk <mateusz.redynk@gmail.com>
2020-10-03 10:24:11 +0200

    cancel token if it's not null

--- lib/PuppeteerSharp/LifecycleWatcher.cs
+++ lib/PuppeteerSharp/LifecycleWatcher.cs
@@ -169,7 +169,7 @@ namespace PuppeteerSharp
             _frameManager.FrameDetached -= OnFrameDetached;
             _frameManager.NetworkManager.Request -= OnRequest;
             _frameManager.Client.Disconnected -= OnClientDisconnected;
-            _terminationCancellationToken.Cancel();
+            _terminationCancellationToken?.Cancel();
             _terminationCancellationToken?.Dispose();
         }
 
