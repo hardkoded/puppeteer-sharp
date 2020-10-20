2c3495bb4dbc46c2ea9d1d9c614e9d40bd2056d8
Jeff Peirson <Jeff.Peirson@a2ius.com>
2020-10-19 21:24:45 -0400

    Using alternate dispose impl

--- lib/PuppeteerSharp/Helpers/SendAsyncResponseQueue.cs
+++ lib/PuppeteerSharp/Helpers/SendAsyncResponseQueue.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -17,9 +16,8 @@ namespace PuppeteerSharp.Helpers
     /// </remarks>
     internal class SendAsyncResponseQueue : IDisposable
     {
-        [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", Justification = "False positive, as it is disposed in Dispose() but after copying to local variable.")]
-        private CancellationTokenSource _disposing;
-
+        private bool _disposed;
+        private readonly List<MessageTask> _pendingTasks;
         private readonly bool _enqueueSendAsyncResponses;
         private readonly ILogger _logger;
 
@@ -27,13 +25,12 @@ namespace PuppeteerSharp.Helpers
         {
             _enqueueSendAsyncResponses = enqueueSendAsyncResponses;
             _logger = logger ?? NullLogger.Instance;
-            _disposing = new CancellationTokenSource();
+            _pendingTasks = new List<MessageTask>();
         }
 
         public void Enqueue(MessageTask callback, ConnectionResponse obj)
         {
-            var disposing = _disposing;
-            if (disposing == null)
+            if (_disposed)
             {
                 throw new ObjectDisposedException(GetType().FullName);
             }
@@ -44,22 +41,53 @@ namespace PuppeteerSharp.Helpers
                 return;
             }
 
-            Task.Run(() => HandleAsyncMessage(callback, obj))
-                .ContinueWith(t =>
+            // Keep a ref to this task until it completes. If it can't finish by the time we dispose this queue,
+            // then we'll find it and cancel it.
+            lock (_pendingTasks)
+            {
+                _pendingTasks.Add(callback);
+            }
+
+            var task = Task.Run(() => HandleAsyncMessage(callback, obj));
+
+            // Unhandled error handler
+            task.ContinueWith(t =>
+            {
+                _logger.LogError(t.Exception, "Failed to complete async handling of SendAsync for {callback}", callback.Method);
+                callback.TaskWrapper.TrySetException(t.Exception!); // t.Exception is available since this runs only on faulted
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            // Always remove from the queue when done, regardless of outcome.
+            task.ContinueWith(_ =>
+            {
+                lock (_pendingTasks)
                 {
-                    _logger.LogError(t.Exception, "Failed to complete async handling of SendAsync for {callback}", callback.Method);
-                    callback.TaskWrapper.TrySetException(t.Exception!); // t.Exception is available since this runs only on faulted
-                }, TaskContinuationOptions.OnlyOnFaulted);
+                    _pendingTasks.Remove(callback);
+                }
+            });
         }
 
         public void Dispose()
         {
-            var cts = Interlocked.CompareExchange(ref _disposing, null, _disposing);
-            if (cts != null)
+            if (_disposed)
             {
-                cts.Cancel();
-                cts.Dispose();
+                return;
             }
+
+            // Ensure all tasks are finished since we're disposing now. Any pending tasks will be canceled.
+            MessageTask[] pendingTasks;
+            lock (_pendingTasks)
+            {
+                pendingTasks = _pendingTasks.ToArray();
+                _pendingTasks.Clear();
+            }
+
+            foreach (var pendingTask in pendingTasks)
+            {
+                pendingTask.TaskWrapper.TrySetCanceled();
+            }
+
+            _disposed = true;
         }
 
         private static void HandleAsyncMessage(MessageTask callback, ConnectionResponse obj)
