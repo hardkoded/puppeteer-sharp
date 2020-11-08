d5044c6fe45542290e83810b9cfd9f49828dc775
jpeirson <xjsmalc@gmail.com>
2020-11-08 06:13:02 -0500

    Addresses deadlock, thread-safety in mixed sync/async apps (#1549)
    * Fixed deadlock, thread-safety in mixed sync/async apps
    
    where if a continuation from CDP.SendAsync in returns calls CDP.SendAsync but uses .GetResult(), then deadlock.
    CDP event messages are still delivered like before, since their order must be preserved. There's potential for deadlock here, but this is still the caller's responsibility to handle events correctly.
    
    * Using expr body like before
    
    * Reverted queue change, added test
    
    * Fixed race in test
    
    * Reverted if branch change
    
    * Reverted change to test
    
    * Reverted dropped unused using
    
    * Added lost BOM from my previous commits to this
    
    * Refactored new SendAsync response handling into its own place
    
    * Possible solution for ensuring async tasks are done
    
    * Addressed false positive disposal warning
    
    * Simplified new task queue
    
    Important part is really only DrainAsync
    
    * Added EnqueueSendAsyncResponses option
    
    similar to EnqueueTransportMessages, to at least provide a way to turn this new async behavior off
    
    * Implemented handling for new EnqueueSendAsyncResponses option
    
    * Temp disabling EnqueueSendAsyncResponses for test run
    
    * Reverted temp change
    
    * Temp reverting CSSCoverage changes
    
    which could be causing tests to hang
    
    * Reverted previous commit
    
    * Ensuring canceled task is handled
    
    * Fixed null ref founding during local test run
    
    * Attempting to roll back regression causing deadlock
    
    * Reverted previous, only logging unhandled exception
    
    * Reverted cancel token param
    
    * Using alternate dispose impl
    
    * Reverted accidental whitespace and BOM changes
    
    * Renames, code cleanup, defaulting new async option to false
    
    * Consolidated into single ConcurrentDictionary
    
    * Renamed test var to match renamed option name
    
    * Modified test to more closely match what was reported in #1354
    
    * Added ENQUEUE_ASYNC_MESSAGES variation
    
    * Second attempt on appveyor matrix
    
    * Ignoring test unless required option is set
    
    * Update lib/PuppeteerSharp/CDPSession.cs
    
    * Update appveyor.yml
    
    Co-authored-by: Jeff Peirson <Jeff.Peirson@a2ius.com>
    Co-authored-by: Darío Kondratiuk <dariokondratiuk@gmail.com>

--- lib/PuppeteerSharp/LaunchOptions.cs
+++ lib/PuppeteerSharp/LaunchOptions.cs
@@ -159,5 +159,17 @@ namespace PuppeteerSharp
         /// Setting this to <c>false</c> proved to work in .NET Core but it tends to fail on .NET Framework.
         /// </remarks>
         public bool EnqueueTransportMessages { get; set; } = true;
+
+        /// <summary>
+        /// Affects how responses to <see cref="CDPSession.SendAsync"/> are returned to the caller. If <c>true</c> (default), the
+        /// response is delivered to the caller on its own thread; otherwise, the response is delivered the same way <see cref="CDPSession.MessageReceived"/>
+        /// events are raised.
+        /// </summary>
+        /// <remarks>
+        /// This should normally be set to <c>true</c> to support applications that aren't <c>async</c> "all the way up"; i.e., the application
+        /// has legacy code that is not async which makes calls into PuppeteerSharp. If you experience issues, or your application is not mixed sync/async use, you
+        /// can set this to <c>false</c> (default).
+        /// </remarks>
+        public bool EnqueueAsyncMessages { get; set; }
     }
 }
