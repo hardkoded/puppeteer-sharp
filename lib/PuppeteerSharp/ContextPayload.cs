d8a3724f721878ef9394ae8660ee09a7675045dc
Gaurav Gupta <g1561998@gmail.com>
2021-01-27 09:21:48 +0530

    Update ContextPayload.cs

--- lib/PuppeteerSharp/ContextPayload.cs
+++ lib/PuppeteerSharp/ContextPayload.cs
@@ -7,8 +7,14 @@ namespace PuppeteerSharp
         /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the context payload aux data.
+        /// </summary>
         public ContextPayloadAuxData AuxData { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
     }
 }
