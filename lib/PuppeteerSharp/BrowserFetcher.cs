ba336870c05132142af175518c4b42da6e8d8c16
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2021-03-07 15:58:06 +0100

    chmod +x nacl_helper (#1643)
    * chmod +x nacl_helper
    
    * Check chmod return code

--- lib/PuppeteerSharp/BrowserFetcher.cs
+++ lib/PuppeteerSharp/BrowserFetcher.cs
@@ -221,10 +221,23 @@ namespace PuppeteerSharp
 
             if (revisionInfo != null && GetCurrentPlatform() == Platform.Linux)
             {
-                int code = LinuxSysCall.Chmod(revisionInfo.ExecutablePath, LinuxSysCall.ExecutableFilePermissions);
-                if (code == -1)
+                string execPath = revisionInfo.ExecutablePath;
+                string dirName = Path.GetDirectoryName(execPath);
+
+                int code = LinuxSysCall.Chmod(execPath, LinuxSysCall.ExecutableFilePermissions);
+                if (code != 0)
                 {
                     throw new Exception("Chmod operation failed");
+                }
+
+                string naclPath = $"{dirName}/nacl_helper";
+                if (File.Exists(naclPath))
+                {
+                    code = LinuxSysCall.Chmod(naclPath, LinuxSysCall.ExecutableFilePermissions);
+                    if (code != 0)
+                    {
+                        throw new Exception("Chmod operation failed");
+                    }
                 }
             }
 
