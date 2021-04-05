e60338a1af735b848de53b1a855f5f3bfbc24606
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2021-04-05 06:56:23 +0200

    Move ConcurrentEnumerator into separate file

---
+++ lib/PuppeteerSharp/Helpers/ConcurrentEnumerator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+// https://stackoverflow.com/a/23446622
+
+internal class ConcurrentEnumerator<T> : IEnumerator<T>
+{
+    #region Fields
+    private readonly IEnumerator<T> _inner;
+    private readonly ReaderWriterLockSlim _lock;
+    #endregion
+
+    #region Constructor
+    public ConcurrentEnumerator(IEnumerable<T> inner, ReaderWriterLockSlim @lock)
+    {
+        this._lock = @lock;
+        this._lock.EnterReadLock();
+        this._inner = inner.GetEnumerator();
+    }
+    #endregion
+
+    #region Methods
+    public bool MoveNext()
+    {
+        return _inner.MoveNext();
+    }
+
+    public void Reset()
+    {
+        _inner.Reset();
+    }
+
+    public void Dispose()
+    {
+        this._lock.ExitReadLock();
+    }
+    #endregion
+
+    #region Properties
+    public T Current
+    {
+        get { return _inner.Current; }
+    }
+
+    object IEnumerator.Current
+    {
+        get { return _inner.Current; }
+    }
+    #endregion
+}
