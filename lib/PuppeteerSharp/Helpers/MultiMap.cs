6d6e8630fa361cfb0067f9b1d3fa54d3c804eb7f
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2021-04-08 03:44:14 +0200

    Use Roslyn.Utilities.ConcurrentSet

--- lib/PuppeteerSharp/Helpers/MultiMap.cs
+++ lib/PuppeteerSharp/Helpers/MultiMap.cs
@@ -6,13 +6,13 @@ namespace PuppeteerSharp.Helpers
 {
     internal class MultiMap<TKey, TValue>
     {
-        private readonly ConcurrentDictionary<TKey, IList<TValue>> _map = new ConcurrentDictionary<TKey, IList<TValue>>();
+        private readonly ConcurrentDictionary<TKey, ICollection<TValue>> _map = new ConcurrentDictionary<TKey, ICollection<TValue>>();
 
         internal void Add(TKey key, TValue value)
-            => _map.GetOrAdd(key, _ => new ConcurrentList<TValue>()).Add(value);
+            => _map.GetOrAdd(key, _ => new ConcurrentSet<TValue>()).Add(value);
 
-        internal IList<TValue> Get(TKey key)
-            => _map.TryGetValue(key, out var set) ? set : new ConcurrentList<TValue>();
+        internal ICollection<TValue> Get(TKey key)
+            => _map.TryGetValue(key, out var set) ? set : new ConcurrentSet<TValue>();
 
         internal bool Has(TKey key, TValue value)
             => _map.TryGetValue(key, out var set) && set.Contains(value);
