02c0599a7e16774ab8b56df6cb1ce7504a50d5bc
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2021-04-09 14:11:07 +0200

    Fix MultiMap concurrency issues (#1687)
    * Fix MultiMap concurrency issues
    
    * Make ConcurrentList internal
    
    * Move ConcurrentEnumerator into separate file
    
    * Satisfy linter
    
    * Use Roslyn.Utilities.ConcurrentSet
    
    * Satisfy linter
    
    * Remove ConcurrentEnumerator

--- lib/PuppeteerSharp/Helpers/MultiMap.cs
+++ lib/PuppeteerSharp/Helpers/MultiMap.cs
@@ -6,13 +6,13 @@ namespace PuppeteerSharp.Helpers
 {
     internal class MultiMap<TKey, TValue>
     {
-        private readonly ConcurrentDictionary<TKey, List<TValue>> _map = new ConcurrentDictionary<TKey, List<TValue>>();
+        private readonly ConcurrentDictionary<TKey, ICollection<TValue>> _map = new ConcurrentDictionary<TKey, ICollection<TValue>>();
 
         internal void Add(TKey key, TValue value)
-            => _map.GetOrAdd(key, _ => new List<TValue>()).Add(value);
+            => _map.GetOrAdd(key, _ => new ConcurrentSet<TValue>()).Add(value);
 
-        internal List<TValue> Get(TKey key)
-            => _map.TryGetValue(key, out var set) ? set : new List<TValue>();
+        internal ICollection<TValue> Get(TKey key)
+            => _map.TryGetValue(key, out var set) ? set : new ConcurrentSet<TValue>();
 
         internal bool Has(TKey key, TValue value)
             => _map.TryGetValue(key, out var set) && set.Contains(value);
