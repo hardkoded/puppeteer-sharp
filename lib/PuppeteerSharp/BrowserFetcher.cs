27aadf04f0a7bf9d6819cd12bef685710e7ff967
Dario Kondratiuk <dariokondratiuk@gmail.com>
2021-05-04 18:19:21 -0300

    Bump firefox version

--- lib/PuppeteerSharp/BrowserFetcher.cs
+++ lib/PuppeteerSharp/BrowserFetcher.cs
@@ -65,7 +65,7 @@ namespace PuppeteerSharp
         /// <summary>
         /// Default Chromium revision.
         /// </summary>
-        public const string DefaultFirefoxRevision = "89.0a1";
+        public const string DefaultFirefoxRevision = "89.0b7";
 
         /// <summary>
         /// Gets the downloads folder.
