e3fb28fccd588c66980a862226ac1a46fb9e3831
Darío Kondratiuk <dariokondratiuk@gmail.com>
2021-08-31 11:12:13 -0300

    Update navigation tests (#1794)

---
+++ lib/PuppeteerSharp.Tests/NavigationTests/PageReloadTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using PuppeteerSharp.Helpers;
+using PuppeteerSharp.Tests.Attributes;
+using PuppeteerSharp.Xunit;
+
+namespace PuppeteerSharp.Tests.NavigationTests
+{
+    [Collection(TestConstants.TestFixtureCollectionName)]
+    public class PageReloadTests : PuppeteerPageBaseTest
+    {
+        public PageReloadTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [PuppeteerTest("navigation.spec.ts", "Page.reload", "should work")]
+        [Fact(Timeout = TestConstants.DefaultTestTimeout)]
+        public async Task ShouldWork()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            await Page.EvaluateFunctionAsync("() => (globalThis._foo = 10)");
+            await Page.ReloadAsync();
+            Assert.Null(await Page.EvaluateFunctionAsync("() => globalThis._foo"));
+        }
+    }
+}
