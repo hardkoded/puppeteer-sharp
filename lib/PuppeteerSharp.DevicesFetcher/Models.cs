efe97dda32cd3cd84eb4086134a44af1cabc00bd
Alex Maitland <amaitland@users.noreply.github.com>
2021-11-11 14:55:58 +1000

    DevicesFetcher - Parse Puppeteer DeviceDescriptors.ts
    - Very basic parsing of DeviceDescriptors.ts
    - Greatly simplified parsing
    - Fixed generator to ouput updated C# class code

--- lib/PuppeteerSharp.DevicesFetcher/Models.cs
+++ lib/PuppeteerSharp.DevicesFetcher/Models.cs
@@ -2,34 +2,32 @@ using Newtonsoft.Json;
 
 namespace PuppeteerSharp.DevicesFetcher
 {
-    public class RootObject
+
+    public class Device
     {
-        public Extension[] Extensions { get; set; }
+        [JsonProperty("userAgent")]
+        public string UserAgent { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("viewport")]
+        public ViewPort Viewport { get; set; }
+    }
 
-        public class Extension
-        {
-            public string Type { get; set; }
-            public Device Device { get; set; }
-        }
+    public class ViewPort
+    {
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
 
-        public class Device
-        {
-            [JsonProperty("show-by-default")]
-            public bool ShowByDefault { get; set; }
-            public string Title { get; set; }
-            public Screen Screen { get; set; }
-            public string[] Capabilities { get; set; }
-            [JsonProperty("user-agent")]
-            public string UserAgent { get; set; }
-            public string Type { get; set; }
-        }
+        [JsonProperty("deviceScaleFactor")]
+        public double DeviceScaleFactor { get; set; }
 
-        public class Screen
-        {
-            public ViewportPayload Horizontal { get; set; }
-            [JsonProperty("device-pixel-ratio")]
-            public double DevicePixelRatio { get; set; }
-            public ViewportPayload Vertical { get; set; }
-        }
+        [JsonProperty("isMobile")]
+        public bool IsMobile { get; set; }
+        [JsonProperty("hasTouch")]
+        public bool HasTouch { get; set; }
+        [JsonProperty("isLandscape")]
+        public bool IsLandscape { get; set; }
     }
 }
