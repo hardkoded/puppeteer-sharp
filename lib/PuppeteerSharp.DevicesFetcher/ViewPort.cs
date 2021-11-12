3637b83f9d2d599dabecfe5bbd9019da3c2cc735
Alex Maitland <amaitland@users.noreply.github.com>
2021-11-12 10:26:35 +1000

    Classes into separate files

--- lib/PuppeteerSharp.DevicesFetcher/ViewPort.cs
+++ lib/PuppeteerSharp.DevicesFetcher/ViewPort.cs
@@ -2,17 +2,6 @@ using Newtonsoft.Json;
 
 namespace PuppeteerSharp.DevicesFetcher
 {
-
-    public class Device
-    {
-        [JsonProperty("userAgent")]
-        public string UserAgent { get; set; }
-        [JsonProperty("name")]
-        public string Name { get; set; }
-        [JsonProperty("viewport")]
-        public ViewPort Viewport { get; set; }
-    }
-
     public class ViewPort
     {
         [JsonProperty("width")]
