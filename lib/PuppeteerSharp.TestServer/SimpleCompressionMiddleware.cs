78c083af1ba6ba03657bfc29a8c19ea85ad80bc1
Darío Kondratiuk <dariokondratiuk@gmail.com>
2022-01-10 10:05:19 -0300

    Fix compression middleware

--- lib/PuppeteerSharp.TestServer/SimpleCompressionMiddleware.cs
+++ lib/PuppeteerSharp.TestServer/SimpleCompressionMiddleware.cs
@@ -40,7 +40,7 @@ namespace PuppeteerSharp.TestServer
                 context.Response.Headers["Content-Encoding"] = "gzip";
                 context.Response.Headers["Content-Length"] = stream.Length.ToString();
                 stream.Position = 0;
-                stream.CopyTo(response);
+                await stream.CopyToAsync(response);
                 context.Response.Body = response;
             }
         }
