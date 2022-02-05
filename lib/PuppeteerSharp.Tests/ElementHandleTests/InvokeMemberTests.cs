ce9f965d8ffec806d2edcdbcedc870e54a670d49
Alex Maitland <amaitland@users.noreply.github.com>
2022-02-05 13:46:21 +1000

    Rollback to 97.1.61
    Was updated for testing and shouldn't have been committed.

---
+++ lib/PuppeteerSharp.Tests/ElementHandleTests/InvokeMemberTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CefSharp.Puppeteer;
+using PuppeteerSharp.Tests.Attributes;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.ElementHandleTests
+{
+    [Collection(TestConstants.TestFixtureCollectionName)]
+    public class InvokeMemberTests : PuppeteerPageBaseTest
+    {
+        public InvokeMemberTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [PuppeteerFact]
+        public async Task ShouldWork()
+        {
+            await DevToolsContext.GoToAsync(TestConstants.ServerUrl + "/input/checkbox.html");
+            var checkbox = await DevToolsContext.QuerySelectorAsync("#agree");
+            await checkbox.InvokeMemberAsync("click");
+
+            var actual = await checkbox.GetPropertyValueAsync<bool>("checked");
+
+            Assert.True(actual);
+        }
+    }
+}
