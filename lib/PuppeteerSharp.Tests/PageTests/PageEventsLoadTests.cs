07fcb3f797396a4742be5253250482c62c1cf9e4
Darío Kondratiuk <dariokondratiuk@gmail.com>
2022-04-26 09:00:05 -0300

    Add missing load test

---
+++ lib/PuppeteerSharp.Tests/PageTests/PageEventsLoadTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using PuppeteerSharp.Tests.Attributes;
+using PuppeteerSharp.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.PageTests
+{
+    [Collection(TestConstants.TestFixtureCollectionName)]
+    public class PageEventsLoadTests : PuppeteerPageBaseTest
+    {
+        public PageEventsLoadTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [PuppeteerTest("page.spec.ts", "Page.Events.Load", "should fire when expected")]
+        [PuppeteerFact]
+        public async Task ShouldFireWhenExpected()
+        {
+            var _ = Page.GoToAsync(TestConstants.AboutBlank);
+            var completion = new TaskCompletionSource<bool>();
+            Page.Load += (_, _) => completion.SetResult(true);
+            await completion.Task;
+        }
+    }
+}
