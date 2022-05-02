36d2b7ba4086e517c5c4f8b46f96a03d60dfe7e8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2022-05-02 08:50:07 -0300

    Add missing frame tests

---
+++ lib/PuppeteerSharp.Tests/FrameTests/FrameEvaluateTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using PuppeteerSharp.Tests.Attributes;
+using PuppeteerSharp.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.FrameTests
+{
+    [Collection(TestConstants.TestFixtureCollectionName)]
+    public class FrameEvaluateTests : PuppeteerPageBaseTest
+    {
+        public FrameEvaluateTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [PuppeteerTest("frame.spec.ts", "Frame.evaluate", "should throw for detached frames")]
+        [PuppeteerFact]
+        public async Task ShouldThrowForDetachedFrames()
+        {
+            await FrameUtils.AttachFrameAsync(Page, "frame1", TestConstants.EmptyPage);
+            var frame = Page.FirstChildFrame();
+            await FrameUtils.DetachFrameAsync(Page, "frame1");
+            var exception = await Assert.ThrowsAnyAsync<PuppeteerException>(
+                () => frame.EvaluateExpressionAsync("5 * 8"));
+            Assert.Contains("Execution Context is not available in detached frame", exception.Message);
+        }
+    }
+}
