e5c7178d95c95f62a94dceb29f86c1a41d42b1ef
Alex Maitland <amaitland@users.noreply.github.com>
2022-05-25 21:38:45 +1000

    ElementHandle - Improve Debugger Display (#1963)
    - Add RemoteObject.ClassName and RemoteObject.Description
    - Add ElementHandle.DebuggerDisplay attribute to use newly added ClassName and Description
    
    Doesn't change any runtime functionality, just improves the debugging output

--- lib/PuppeteerSharp/ElementHandle.cs
+++ lib/PuppeteerSharp/ElementHandle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace PuppeteerSharp
     /// Inherits from <see cref="JSHandle"/>. It represents an in-page DOM element.
     /// ElementHandles can be created by <see cref="PuppeteerSharp.Page.QuerySelectorAsync(string)"/> or <see cref="PuppeteerSharp.Page.QuerySelectorAllAsync(string)"/>.
     /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class ElementHandle : JSHandle
     {
         private readonly FrameManager _frameManager;
@@ -32,6 +34,9 @@ namespace PuppeteerSharp
             _frameManager = frameManager;
             _logger = client.LoggerFactory.CreateLogger<ElementHandle>();
         }
+
+        private string DebuggerDisplay =>
+            string.IsNullOrEmpty(RemoteObject.ClassName) ? ToString() : $"{RemoteObject.ClassName}@{RemoteObject.Description}";
 
         internal Page Page { get; }
 
