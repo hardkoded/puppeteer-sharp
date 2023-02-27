47f0a4c5ca51758b35e14d66d55cef82b7b14ce5
Jonas Nyrup <jnyrup@users.noreply.github.com>
2023-02-27 14:31:32 +0100

    Fix duplicated test (#2092)
    ShouldWorkWithFakeClickingTargetBlankAndReNoopener and ShouldWorkWithFakeClickingTargetBlankAndRelNoopener were identical.
    The first one should use 'rel=opener'
    
    https://github.com/puppeteer/puppeteer/blob/415da9230066e5b399f60963b639bfbafdc8ec57/test/src/page.spec.ts#L296-L346

--- lib/PuppeteerSharp.Tests/PageTests/PageEventsPopupTests.cs
+++ lib/PuppeteerSharp.Tests/PageTests/PageEventsPopupTests.cs
@@ -63,10 +63,10 @@ namespace PuppeteerSharp.Tests.PageTests
 
         [PuppeteerTest("page.spec.ts", "Page.Events.Popup", "should work with clicking target=_blank and with rel=opener")]
         [SkipBrowserFact(skipFirefox: true)]
-        public async Task ShouldWorkWithFakeClickingTargetBlankAndReNoopener()
+        public async Task ShouldWorkWithFakeClickingTargetBlankAndRelOpener()
         {
             await Page.GoToAsync(TestConstants.EmptyPage);
-            await Page.SetContentAsync("<a target=_blank rel=noopener href='/one-style.html'>yo</a>");
+            await Page.SetContentAsync("<a target=_blank rel=opener href='/one-style.html'>yo</a>");
 
             var popupTaskSource = new TaskCompletionSource<IPage>();
             Page.Popup += (_, e) => popupTaskSource.TrySetResult(e.PopupPage);
@@ -76,7 +76,7 @@ namespace PuppeteerSharp.Tests.PageTests
                 Page.QuerySelectorAsync("a").EvaluateFunctionAsync("a => a.click()"));
 
             Assert.False(await Page.EvaluateExpressionAsync<bool>("!!window.opener"));
-            Assert.False(await popupTaskSource.Task.Result.EvaluateExpressionAsync<bool>("!!window.opener"));
+            Assert.True(await popupTaskSource.Task.Result.EvaluateExpressionAsync<bool>("!!window.opener"));
         }
 
         [PuppeteerTest("page.spec.ts", "Page.Events.Popup", "should work with fake-clicking target=_blank and rel=noopener")]
