926112a98b77c34f640a7d129511b2673d2053ef
Jonas Nyrup <jnyrup@users.noreply.github.com>
2023-04-09 03:10:46 +0200

    TaskQueue tests (#2127)
    * Remove unneeded suppressions
    
    With #2123 we now dispose the semaphore as expected by the analyzer
    
    * Add unit tests of Dispose methods on TaskQueue

---
+++ lib/PuppeteerSharp.Tests/TaskQueueTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using PuppeteerSharp.Helpers;
+using Xunit;
+
+namespace PuppeteerSharp.Tests
+{
+    public class TaskQueueTests
+    {
+        [Fact]
+        public void ShouldDisposeSemaphoreWhenDisposing()
+        {
+            var taskQueue = new TaskQueue();
+            taskQueue.Dispose();
+
+            var semaphore = GetSemaphore(taskQueue);
+            Assert.Throws<ObjectDisposedException>(() => semaphore.AvailableWaitHandle);
+        }
+
+        [Fact]
+        public void ShouldNotThrowWhenDisposingMultipleTimes()
+        {
+            var taskQueue = new TaskQueue();
+            taskQueue.Dispose();
+
+            // Can safely be disposed a second time
+            taskQueue.Dispose();
+        }
+
+        [Fact]
+        public async Task ShouldDisposeSemaphoreWhenDisposingAsync()
+        {
+            var taskQueue = new TaskQueue();
+            await taskQueue.DisposeAsync().ConfigureAwait(false);
+
+            var semaphore = GetSemaphore(taskQueue);
+            Assert.Throws<ObjectDisposedException>(() => semaphore.AvailableWaitHandle);
+        }
+
+        [Fact]
+        public async Task ShouldNotThrowWhenDisposingMultipleTimesAsync()
+        {
+            var taskQueue = new TaskQueue();
+            await taskQueue.DisposeAsync().ConfigureAwait(false);
+
+            // Can safely be disposed a second time
+            await taskQueue.DisposeAsync().ConfigureAwait(false);
+        }
+
+        private static SemaphoreSlim GetSemaphore(TaskQueue queue) =>
+            (SemaphoreSlim)typeof(TaskQueue).GetField("_semaphore", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(queue);
+    }
+}
