5460ec3f713c3eb26bf0f73600202850e565e79d
Darío Kondratiuk <dariokondratiuk@gmail.com>
2023-04-10 12:08:47 -0300

    Add injected framework (#2122)
    * Add injected framework
    
    * Remove bad using

---
+++ lib/PuppeteerSharp.Tests/InjectedTests/InjectedTests.cs
@@ -0,0 +1,28 @@
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using PuppeteerSharp.Tests.Attributes;
+using PuppeteerSharp.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PuppeteerSharp.Tests.InjectedTests
+{
+    [Collection(TestConstants.TestFixtureCollectionName)]
+    public class InjectedTests : PuppeteerPageBaseTest
+    {
+        public InjectedTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [PuppeteerTest("injected.spec.ts", "InjectedUtil tests", "should work")]
+        [PuppeteerFact]
+        public async Task ShouldWork()
+        {
+            var result = await (Page.MainFrame as Frame)
+                .SecondaryWorld.EvaluateFunctionAsync<bool>(@"() => {
+                      return typeof InjectedUtil === 'object';
+                  }");
+            Assert.True(result);
+        }
+    }
+}
---
+++ lib/PuppeteerSharp/Injected/README.md
@@ -0,0 +1,7 @@
+# Injected
+
+This folder contains the same script that puppetter builds when it releases a new version.
+It's not the responsibility of this library to build the injected.js file.
+We just need to grab it from upstream.
+
+TODO: Do this automatically :)
