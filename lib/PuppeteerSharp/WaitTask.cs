0c4ccc72cac532e96cea6f1eaa4ccfcb06fa7aab
Alex Maitland <amaitland@users.noreply.github.com>
2023-04-25 17:31:05 +1000

    Fix Evaluation failed: TypeError: fun is not a function
    - The whitespace is causing the new Function() call to fail.

--- lib/PuppeteerSharp/WaitTask.cs
+++ lib/PuppeteerSharp/WaitTask.cs
@@ -133,7 +133,7 @@ namespace PuppeteerSharp
                     _poller = await _isolatedWorld.EvaluateFunctionHandleAsync(
                             @"
                             ({MutationPoller, createFunction}, root, fn, ...args) => {
-                                const fun = createFunction(fn);
+                                const fun = createFunction(fn.trim());
                                 return new MutationPoller(() => {
                                     return fun(...args);
                                 }, root || document);
