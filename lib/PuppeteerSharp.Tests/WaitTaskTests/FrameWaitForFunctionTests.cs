d2c13265c4f4e927f18ab15b691f3036b5fe05d1
Leonardo Fernandes <leonardo.monteiro.fernandes@gmail.com>
2023-06-12 16:45:12 +1000

    Fixed FrameWaitForFunctionTests by awaiting for the call to `poller.start()`.
    The previous attempt still did not guarantee the correct execution, but this should.

--- lib/PuppeteerSharp.Tests/WaitTaskTests/FrameWaitForFunctionTests.cs
+++ lib/PuppeteerSharp.Tests/WaitTaskTests/FrameWaitForFunctionTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using PuppeteerSharp.Tests.Attributes;
+using PuppeteerSharp.Transport;
 using PuppeteerSharp.Xunit;
 using Xunit;
 using Xunit.Abstractions;
@@ -8,10 +9,23 @@ using Xunit.Abstractions;
 namespace PuppeteerSharp.Tests.WaitTaskTests
 {
     [Collection(TestConstants.TestFixtureCollectionName)]
-    public class FrameWaitForFunctionTests : PuppeteerPageBaseTest
+    public sealed class FrameWaitForFunctionTests : PuppeteerPageBaseTest, IDisposable
     {
+        private ConnectionTransportInterceptor _connectionTransportInterceptor;
+
         public FrameWaitForFunctionTests(ITestOutputHelper output) : base(output)
         {
+            DefaultOptions = TestConstants.DefaultBrowserOptions();
+            DefaultOptions.TransportFactory = async (url, options, cancellationToken) =>
+            {
+                _connectionTransportInterceptor = new ConnectionTransportInterceptor(await WebSocketTransport.DefaultTransportFactory(url, options, cancellationToken));
+                return _connectionTransportInterceptor;
+            };
+        }
+
+        public void Dispose()
+        {
+            _connectionTransportInterceptor.Dispose();
         }
 
         [PuppeteerTest("waittask.spec.ts", "Frame.waitForFunction", "should work when resolved right before execution context disposal")]
@@ -33,22 +47,24 @@ namespace PuppeteerSharp.Tests.WaitTaskTests
         {
             var startTime = DateTime.UtcNow;
             var polling = 100;
+            var startedPolling = ListenForStartPolling();
             var watchdog = Page.WaitForFunctionAsync("() => window.__FOO === 'hit'", new WaitForFunctionOptions { PollingInterval = polling });
-            await EnsureWaitForFunctionStartedPolling();
+            await startedPolling.Task;
             await Page.EvaluateFunctionAsync("() => setTimeout(window.__FOO = 'hit', 50)");
             await watchdog;
 
             Assert.True((DateTime.UtcNow - startTime).TotalMilliseconds > polling / 2);
         }
-        
+
         [PuppeteerTest("waittask.spec.ts", "Frame.waitForFunction", "should poll on interval async")]
         [PuppeteerFact]
         public async Task ShouldPollOnIntervalAsync()
         {
             var startTime = DateTime.UtcNow;
             var polling = 1000;
+            var startedPolling = ListenForStartPolling();
             var watchdog = Page.WaitForFunctionAsync("async () => window.__FOO === 'hit'", new WaitForFunctionOptions { PollingInterval = polling });
-            await EnsureWaitForFunctionStartedPolling();
+            await startedPolling.Task;
             await Page.EvaluateFunctionAsync("async () => setTimeout(window.__FOO = 'hit', 50)");
             await watchdog;
             Assert.True((DateTime.UtcNow - startTime).TotalMilliseconds > polling / 2);
@@ -59,10 +75,11 @@ namespace PuppeteerSharp.Tests.WaitTaskTests
         public async Task ShouldPollOnMutation()
         {
             var success = false;
+            var startedPolling = ListenForStartPolling();
             var watchdog = Page.WaitForFunctionAsync("() => window.__FOO === 'hit'",
                 new WaitForFunctionOptions { Polling = WaitForFunctionPollingOption.Mutation })
                 .ContinueWith(_ => success = true);
-            await EnsureWaitForFunctionStartedPolling();
+            await startedPolling.Task;
             await Page.EvaluateExpressionAsync("window.__FOO = 'hit'");
             Assert.False(success);
             await Page.EvaluateExpressionAsync("document.body.appendChild(document.createElement('div'))");
@@ -74,10 +91,11 @@ namespace PuppeteerSharp.Tests.WaitTaskTests
         public async Task ShouldPollOnMutationAsync()
         {
             var success = false;
+            var startedPolling = ListenForStartPolling();
             var watchdog = Page.WaitForFunctionAsync("async () => window.__FOO === 'hit'",
                 new WaitForFunctionOptions { Polling = WaitForFunctionPollingOption.Mutation })
                 .ContinueWith(_ => success = true);
-            await EnsureWaitForFunctionStartedPolling();
+            await startedPolling.Task;
             await Page.EvaluateFunctionAsync("async () => window.__FOO = 'hit'");
             Assert.False(success);
             await Page.EvaluateExpressionAsync("document.body.appendChild(document.createElement('div'))");
@@ -215,11 +233,58 @@ namespace PuppeteerSharp.Tests.WaitTaskTests
             await watchdog;
         }
 
-        private async Task EnsureWaitForFunctionStartedPolling()
+        private TaskCompletionSource<bool> ListenForStartPolling()
         {
+            TaskCompletionSource<bool> startedPolling = new TaskCompletionSource<bool>();
+
             // Wait for function will release the execution faster than in node.
-            // We add some CDP action to wait for the task to start the polling
-            await Page.EvaluateExpressionAsync("null");
+            // We intercept the poller.start() call to prevent tests from continuing before the polling has started.
+            _connectionTransportInterceptor.MessageSent += (_, message) =>
+            {
+                if (message.Contains("poller => poller.start()"))
+                {
+                    startedPolling.SetResult(true);
+                }
+            };
+
+            return startedPolling;
+        }
+
+        private sealed class ConnectionTransportInterceptor : IConnectionTransport
+        {
+            private readonly IConnectionTransport _connectionTransport;
+
+            public ConnectionTransportInterceptor(IConnectionTransport connectionTransport)
+            {
+                _connectionTransport = connectionTransport;
+            }
+
+            public event EventHandler<string> MessageSent;
+
+            public Task SendAsync(string message)
+            {
+                var task = _connectionTransport.SendAsync(message);
+                MessageSent?.Invoke(_connectionTransport, message);
+                return task;
+            }
+
+            public bool IsClosed => _connectionTransport.IsClosed;
+
+            public event EventHandler<TransportClosedEventArgs> Closed
+            {
+                add { _connectionTransport.Closed += value; }
+                remove { _connectionTransport.Closed -= value; }
+            }
+
+            public event EventHandler<MessageReceivedEventArgs> MessageReceived
+            {
+                add { _connectionTransport.MessageReceived += value; }
+                remove { _connectionTransport.MessageReceived -= value; }
+            }
+
+            public void Dispose() => _connectionTransport.Dispose();
+
+            public void StopReading() => _connectionTransport.StopReading();
         }
     }
 }
