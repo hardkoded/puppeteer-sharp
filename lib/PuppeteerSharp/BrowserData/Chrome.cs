0c6d65324a1b0935b9241aeb826d5a9f7ba1734c
Darío Kondratiuk <dariokondratiuk@gmail.com>
2023-10-06 09:25:37 -0300

    Roll chrome to 117.0.5938.149

--- lib/PuppeteerSharp/BrowserData/Chrome.cs
+++ lib/PuppeteerSharp/BrowserData/Chrome.cs
@@ -13,7 +13,7 @@ namespace PuppeteerSharp.BrowserData
         /// <summary>
         /// Default chrome build.
         /// </summary>
-        public static string DefaultBuildId => "117.0.5938.62";
+        public static string DefaultBuildId => "117.0.5938.149";
 
         internal static async Task<string> ResolveBuildIdAsync(ChromeReleaseChannel channel)
             => (await GetLastKnownGoodReleaseForChannel(channel).ConfigureAwait(false)).Version;
