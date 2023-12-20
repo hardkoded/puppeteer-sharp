20c3befa0f8368de7be0f6964940144213ebba6c
Darío Kondratiuk <dariokondratiuk@gmail.com>
2023-12-20 16:59:55 -0300

    docs: How to download different browsers (#2383)
    * Add downloads example
    
    * Add docs

--- lib/PuppeteerSharp.Tests/Browsers/Chrome/ChromeDataTests.cs
+++ lib/PuppeteerSharp.Tests/Browsers/Chrome/ChromeDataTests.cs
@@ -10,6 +10,46 @@ namespace PuppeteerSharp.Tests.Browsers.Chrome
 {
     public class ChromeDataTests
     {
+        public async Task Usage()
+        {
+            #region CustomVersionsExample
+            Console.WriteLine("Downloading browsers");
+
+            using var browserFetcher = new BrowserFetcher(SupportedBrowser.Chrome);
+            var chrome118 = await browserFetcher.DownloadAsync("118.0.5993.70");
+            var chrome119 = await browserFetcher.DownloadAsync("119.0.5997.0");
+
+            Console.WriteLine("Navigating");
+            await using (var browser = await Puppeteer.LaunchAsync(new()
+                         {
+                             ExecutablePath = chrome118.GetExecutablePath(),
+                         }))
+            {
+                await using var page = await browser.NewPageAsync();
+                await page.GoToAsync("https://www.whatismybrowser.com/");
+
+                Console.WriteLine("Generating PDF");
+                await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "118.pdf"));
+
+                Console.WriteLine("Export completed");
+            }
+
+            await using (var browser = await Puppeteer.LaunchAsync(new()
+                         {
+                             ExecutablePath = chrome119.GetExecutablePath(),
+                         }))
+            {
+                await using var page = await browser.NewPageAsync();
+                await page.GoToAsync("https://www.whatismybrowser.com/");
+
+                Console.WriteLine("Generating PDF");
+                await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "119.pdf"));
+
+                Console.WriteLine("Export completed");
+            }
+            #endregion
+        }
+
         [PuppeteerTest("chrome-data.spec.ts", "Chrome", "should resolve download URLs")]
         public void ShouldResolveDownloadUrls()
         {
