09f044ffee0c3cf7b6e7b7c98893e54beb5fac36
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-03-08 13:29:59 -0300

    Fix devtools tests

--- lib/PuppeteerSharp.Tests/DevtoolsTests/DevtoolsTests.cs
+++ lib/PuppeteerSharp.Tests/DevtoolsTests/DevtoolsTests.cs
@@ -36,6 +36,32 @@ namespace PuppeteerSharp.Tests.DevtoolsTests
             Assert.True(await page.EvaluateExpressionAsync<bool>("Boolean(DevToolsAPI)"));
         }
 
+        [Test, Retry(2),
+         PuppeteerTest("devtools.spec", "DevTools",
+             "target.page() should return a DevTools page if custom isPageTarget is provided")]
+        public async Task TargetPageShouldReturnADevToolsPageIfCustomIsPageTargetIsProvided()
+        {
+            var headfulOptions = TestConstants.DefaultBrowserOptions();
+            headfulOptions.Devtools = true;
+
+            await using var originalBrowser = await Puppeteer.LaunchAsync(
+                headfulOptions,
+                TestConstants.LoggerFactory);
+            var browserWSEndpoint = originalBrowser.WebSocketEndpoint;
+            await using var browser = await Puppeteer.ConnectAsync(new ConnectOptions
+            {
+                BrowserWSEndpoint = browserWSEndpoint,
+                TargetFilter = target => target.Type == TargetType.Other && target.Url.StartsWith("devtools://")
+            }, TestConstants.LoggerFactory);
+            var devtoolsTargetTask = browser.WaitForTargetAsync(t => t.Type == TargetType.Other);
+            await browser.NewPageAsync();
+            var devtoolsTarget = await devtoolsTargetTask;
+            await using var page = await devtoolsTarget.PageAsync();
+            Assert.AreEqual(6, await page.EvaluateFunctionAsync<int>("() => 2 * 3"));
+            var pages = await browser.PagesAsync();
+            Assert.True(pages.Contains(page));
+        }
+
         [Test, Retry(2), PuppeteerTest("devtools.spec", "DevTools", "target.page() should return a DevTools page if asPage is used")]
         public async Task TargetPageShouldReturnADevToolsPageIfAsPageIsUsed()
         {
@@ -51,7 +77,7 @@ namespace PuppeteerSharp.Tests.DevtoolsTests
             var devtoolsTarget = await devtoolsTargetTask;
             await using var page = await devtoolsTarget.AsPageAsync();
             Assert.AreEqual(6, await page.EvaluateFunctionAsync<int>("() => 2 * 3"));
-            Assert.True((await browser.PagesAsync()).Contains(page));
+            Assert.False((await browser.PagesAsync()).Contains(page));
         }
 
     }
