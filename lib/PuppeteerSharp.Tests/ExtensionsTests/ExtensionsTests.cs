963dad761f97f1dc2c7b812209f4e726bc162673
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-03-20 11:16:30 -0300

    Move some tests to extensions

---
+++ lib/PuppeteerSharp.Tests/ExtensionsTests/ExtensionsTests.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PuppeteerSharp.Helpers;
+using PuppeteerSharp.Messaging;
+using PuppeteerSharp.Nunit;
+
+namespace PuppeteerSharp.Tests.ExtensionsTests
+{
+    public class ExtensionsTests : PuppeteerBaseTest
+    {
+        [Test, Retry(2), PuppeteerTest("extensions.spec", "extensions", "background_page target type should be available")]
+        public async Task BackgroundPageTargetTypeShouldBeAvailable()
+        {
+            await using var browserWithExtension = await Puppeteer.LaunchAsync(
+                TestConstants.BrowserWithExtensionOptions(),
+                TestConstants.LoggerFactory);
+            await using (await browserWithExtension.NewPageAsync())
+            {
+                var backgroundPageTarget = await browserWithExtension.WaitForTargetAsync(t => t.Type == TargetType.BackgroundPage);
+                Assert.NotNull(backgroundPageTarget);
+            }
+        }
+
+        [Test, Retry(2), PuppeteerTest("extensions.spec", "extensions", "target.page() should return a background_page")]
+        public async Task TargetPageShouldReturnABackgroundPage()
+        {
+            await using var browserWithExtension = await Puppeteer.LaunchAsync(
+                TestConstants.BrowserWithExtensionOptions(),
+                TestConstants.LoggerFactory);
+            var backgroundPageTarget = await browserWithExtension.WaitForTargetAsync(t => t.Type == TargetType.BackgroundPage);
+            await using var page = await backgroundPageTarget.PageAsync();
+            Assert.AreEqual(6, await page.EvaluateFunctionAsync<int>("() => 2 * 3"));
+            Assert.AreEqual(42, await page.EvaluateFunctionAsync<int>("() => window.MAGIC"));
+        }
+    }
+}
--- lib/PuppeteerSharp.Tests/HeadfulTests/HeadfulTests.cs
+++ lib/PuppeteerSharp.Tests/HeadfulTests/HeadfulTests.cs
@@ -24,32 +24,6 @@ namespace PuppeteerSharp.Tests.HeadfulTests
             };
         }
 
-        [Test, Retry(2), PuppeteerTest("headful.spec", "HEADFUL", "background_page target type should be available")]
-        public async Task BackgroundPageTargetTypeShouldBeAvailable()
-        {
-            await using var browserWithExtension = await Puppeteer.LaunchAsync(
-                TestConstants.BrowserWithExtensionOptions(),
-                TestConstants.LoggerFactory);
-            await using (await browserWithExtension.NewPageAsync())
-            {
-                var backgroundPageTarget = await browserWithExtension.WaitForTargetAsync(t => t.Type == TargetType.BackgroundPage);
-                Assert.NotNull(backgroundPageTarget);
-            }
-        }
-
-        [Test, Retry(2), PuppeteerTest("headful.spec", "HEADFUL", "target.page() should return a background_page")]
-        [Ignore("Marked as Fail/Pass upstream")]
-        public async Task TargetPageShouldReturnABackgroundPage()
-        {
-            await using var browserWithExtension = await Puppeteer.LaunchAsync(
-                TestConstants.BrowserWithExtensionOptions(),
-                TestConstants.LoggerFactory);
-            var backgroundPageTarget = await browserWithExtension.WaitForTargetAsync(t => t.Type == TargetType.BackgroundPage);
-            await using var page = await backgroundPageTarget.PageAsync();
-            Assert.AreEqual(6, await page.EvaluateFunctionAsync<int>("() => 2 * 3"));
-            Assert.AreEqual(42, await page.EvaluateFunctionAsync<int>("() => window.MAGIC"));
-        }
-
         [Test, Retry(2), PuppeteerTest("headful.spec", "HEADFUL", "should have default url when launching browser")]
         public async Task ShouldHaveDefaultUrlWhenLaunchingBrowser()
         {
