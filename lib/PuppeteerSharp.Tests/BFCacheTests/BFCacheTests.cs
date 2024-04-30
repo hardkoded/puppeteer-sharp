2eaba4893b39eed6bc544346447bd0efedfc40d9
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-04-30 12:16:07 -0300

    Add missing BF cache test (#2619)

--- lib/PuppeteerSharp.Tests/BFCacheTests/BFCacheTests.cs
+++ lib/PuppeteerSharp.Tests/BFCacheTests/BFCacheTests.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using NUnit.Framework;
+using PuppeteerSharp.Helpers;
 using PuppeteerSharp.Nunit;
 
 namespace PuppeteerSharp.Tests.BFCacheTests;
@@ -22,5 +23,27 @@ public class BFCacheTests : PuppeteerPageBaseTest
         StringAssert.Contains("target", Page.Url);
         await Task.WhenAll(Page.WaitForNavigationAsync(), Page.GoBackAsync());
         Assert.AreEqual("BFCachednext", await Page.EvaluateExpressionAsync<string>("document.body.innerText"));
+    }
+
+    [Test, Retry(2), PuppeteerTest("bfcache.spec", "BFCache", "can navigate to a BFCached page containing an OOPIF and a worker")]
+    public async Task CanNavigateToABFCachedPageContainingAnOOPIFAndAWorker()
+    {
+        Page.DefaultTimeout = 30_000;
+        var workerTcs = new TaskCompletionSource<WebWorker>();
+        Page.WorkerCreated += (_, e) => workerTcs.TrySetResult(e.Worker);
+        await Page.GoToAsync(TestConstants.ServerUrl + "/cached/bfcache/worker-iframe-container.html");
+        var worker1 = await workerTcs.Task.WithTimeout();
+        Assert.AreEqual(2, await worker1.EvaluateExpressionAsync<int>("1 + 1"));
+        await Task.WhenAll(
+            Page.WaitForNavigationAsync(),
+            Page.ClickAsync("a"));
+
+        workerTcs = new TaskCompletionSource<WebWorker>();
+        await Task.WhenAll(
+            Page.WaitForNavigationAsync(),
+            Page.GoBackAsync()).WithTimeout();
+
+        var worker2 = await workerTcs.Task.WithTimeout();
+        Assert.AreEqual(2, await worker2.EvaluateExpressionAsync<int>("1 + 1"));
     }
 }
