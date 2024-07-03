6d1a9d8cbe60bb829c3a8fa663785bb890e9e796
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-07-03 11:50:20 -0300

    Update lib/PuppeteerSharp/Cdp/CdpBrowser.cs

--- lib/PuppeteerSharp/Cdp/CdpBrowser.cs
+++ lib/PuppeteerSharp/Cdp/CdpBrowser.cs
@@ -318,10 +318,11 @@ public class CdpBrowser : Browser
                     var closeTimeout = TimeSpan.FromMilliseconds(CloseTimeout);
                     await Launcher.EnsureExitAsync(closeTimeout).ConfigureAwait(false);
 
-                    // Now we can safely await the browser close operation without risking keeping chromium
-                    // process running for indeterminate period.
-                    await browserCloseTask.ConfigureAwait(false);
                 }
+                
+                // Now we can safely await the browser close operation without risking keeping chromium
+                // process running for indeterminate period.
+                await browserCloseTask.ConfigureAwait(false);
             }
             finally
             {
