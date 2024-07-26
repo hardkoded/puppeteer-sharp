52e9cd8aec8d86f3660b2dca08715eebd6cac487
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-07-26 14:18:11 -0300

    Add enum member to MouseButton

--- lib/PuppeteerSharp/Input/MouseButton.cs
+++ lib/PuppeteerSharp/Input/MouseButton.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace PuppeteerSharp.Input
 {
@@ -13,31 +14,37 @@ namespace PuppeteerSharp.Input
         /// <summary>
         /// Non specified.
         /// </summary>
+        [EnumMember(Value = "none")]
         None = 0,
 
         /// <summary>
         /// The left mouse button.
         /// </summary>
+        [EnumMember(Value = "left")]
         Left = 1,
 
         /// <summary>
         /// The right mouse button.
         /// </summary>
+        [EnumMember(Value = "right")]
         Right = 1 << 1,
 
         /// <summary>
         /// The middle mouse button.
         /// </summary>
+        [EnumMember(Value = "middle")]
         Middle = 1 << 2,
 
         /// <summary>
         /// The back mouse button.
         /// </summary>
+        [EnumMember(Value = "back")]
         Back = 1 << 3,
 
         /// <summary>
         /// The forward mouse button.
         /// </summary>
+        [EnumMember(Value = "forward")]
         Forward = 1 << 4,
     }
 }
