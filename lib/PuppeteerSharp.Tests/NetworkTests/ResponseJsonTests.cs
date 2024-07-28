abfe4c44246740f32e3ffbc3d6aae34fb62028b3
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-07-28 18:49:59 -0300

    Fix ResponseJsonTests

--- lib/PuppeteerSharp.Tests/NetworkTests/ResponseJsonTests.cs
+++ lib/PuppeteerSharp.Tests/NetworkTests/ResponseJsonTests.cs
@@ -7,15 +7,11 @@ namespace PuppeteerSharp.Tests.NetworkTests
 {
     public class ResponseJsonTests : PuppeteerPageBaseTest
     {
-        public ResponseJsonTests() : base()
-        {
-        }
-
         [Test, Retry(2), PuppeteerTest("network.spec", "network Response.json", "should work")]
         public async Task ShouldWork()
         {
             var response = await Page.GoToAsync(TestConstants.ServerUrl + "/simple.json");
-            Assert.AreEqual(JsonDocument.Parse("""{"foo": "bar"}"""), await response.JsonAsync());
+            Assert.AreEqual("bar", (await response.JsonAsync<JsonElement>()).GetProperty("foo").GetString());
         }
     }
 }
