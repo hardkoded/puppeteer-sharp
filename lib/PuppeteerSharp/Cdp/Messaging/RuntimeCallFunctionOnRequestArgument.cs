8c9b807f2508eb8f7e15bb516f4e88c2502c539d
Darío Kondratiuk <dariokondratiuk@gmail.com>
2024-07-30 19:05:54 -0300

    Fix evaluate with nulls

--- lib/PuppeteerSharp/Cdp/Messaging/RuntimeCallFunctionOnRequestArgument.cs
+++ lib/PuppeteerSharp/Cdp/Messaging/RuntimeCallFunctionOnRequestArgument.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PuppeteerSharp.Cdp.Messaging
 {
     internal class RuntimeCallFunctionOnRequestArgument
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
         public object Value { get; set; }
     }
 }
