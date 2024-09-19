e14f59da8d95536e0002efa13f451f2dc5c1c762
Aleksey <942091+AlexCSDev@users.noreply.github.com>
2024-09-19 17:59:31 +0300

    Fix deserialization exception when retrieving cookie with PartitionKey set (#2783)
    * Fix incorrect CookieParam.PartitionKey type which caused deserialization exceptions
    
    * Reimplement fix based on upstream solution
    
    * Delete CookiePartitionKey
    
    * remove nullable and bump version
    
    ---------
    
    Co-authored-by: Darío Kondratiuk <dariokondratiuk@gmail.com>

---
+++ lib/PuppeteerSharp/Helpers/Json/CookiePartitionKeyConverter.cs
@@ -0,0 +1,41 @@
+#nullable enable
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace PuppeteerSharp.Helpers.Json
+{
+    internal sealed class CookiePartitionKeyConverter : JsonConverter<string>
+    {
+        /// <inheritdoc cref="JsonConverter"/>
+        public override bool CanConvert(Type objectType) => typeof(string).IsAssignableFrom(objectType);
+
+        /// <inheritdoc cref="JsonConverter"/>
+        public override string? Read(
+            ref Utf8JsonReader reader,
+            Type objectType,
+            JsonSerializerOptions options)
+        {
+            JsonNode? node = JsonNode.Parse(ref reader);
+
+            return node?["topLevelSite"]?.GetValue<string>() ?? null;
+        }
+
+        /// <inheritdoc cref="JsonConverter"/>
+        public override void Write(
+            Utf8JsonWriter writer,
+            string value,
+            JsonSerializerOptions options)
+        {
+            if (value != null && writer != null)
+            {
+                writer.WriteStartObject("partitionKey");
+                writer.WriteString("topLevelSite", value);
+                writer.WriteBoolean("hasCrossSiteAncestor", false);
+                writer.WriteEndObject();
+            }
+        }
+    }
+}
