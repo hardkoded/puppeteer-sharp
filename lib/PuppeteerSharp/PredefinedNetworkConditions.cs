76829b0ad09c6c590d6ba7345d1032952ee77b39
campersau <buchholz.bastian@googlemail.com>
2024-09-29 10:24:35 +0200

    Add Slow 4G / Fast 4G predefined network conditions

--- lib/PuppeteerSharp/PredefinedNetworkConditions.cs
+++ lib/PuppeteerSharp/PredefinedNetworkConditions.cs
@@ -13,15 +13,50 @@ namespace PuppeteerSharp
         {
             [NetworkConditions.Slow3G] = new NetworkConditions
             {
+                // ~500Kbps down
                 Download = ((500 * 1000) / 8) * 0.8,
+
+                // ~500Kbps up
                 Upload = ((500 * 1000) / 8) * 0.8,
+
+                // 400ms RTT
                 Latency = 400 * 5,
             },
             [NetworkConditions.Fast3G] = new NetworkConditions
             {
+                // ~1.6 Mbps down
                 Download = ((1.6 * 1000 * 1000) / 8) * 0.9,
+
+                // ~0.75 Mbps up
                 Upload = ((750 * 1000) / 8) * 0.9,
+
+                // 150ms RTT
                 Latency = 150 * 3.75,
+            },
+
+            // alias to Fast 3G to align with Lighthouse (crbug.com/342406608)
+            // and DevTools (crbug.com/342406608),
+            [NetworkConditions.Slow4G] = new NetworkConditions
+            {
+                // ~1.6 Mbps down
+                Download = ((1.6 * 1000 * 1000) / 8) * 0.9,
+
+                // ~0.75 Mbps up
+                Upload = ((750 * 1000) / 8) * 0.9,
+
+                // 150ms RTT
+                Latency = 150 * 3.75,
+            },
+            [NetworkConditions.Fast4G] = new NetworkConditions
+            {
+                // 9 Mbps down
+                Download = ((9 * 1000 * 1000) / 8) * 0.9,
+
+                // 1.5 Mbps up
+                Upload = ((1.5 * 1000 * 1000) / 8) * 0.9,
+
+                // 60ms RTT
+                Latency = 60 * 2.75,
             },
         };
 
