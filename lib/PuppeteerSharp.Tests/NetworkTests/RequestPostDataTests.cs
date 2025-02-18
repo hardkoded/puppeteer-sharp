86148415068e3804fe599682fb1f1d71c1c80203
Darío Kondratiuk <dariokondratiuk@gmail.com>
2025-02-18 17:04:47 -0300

    Fix low surrogate on post data (#2887)
    * Fix low surrogate on post data
    
    * bump version
    
    * fix and tests
    
    * ignore some tests in firefox
    
    * fix test
    
    * fallback to string

--- lib/PuppeteerSharp.Tests/NetworkTests/RequestPostDataTests.cs
+++ lib/PuppeteerSharp.Tests/NetworkTests/RequestPostDataTests.cs
@@ -19,6 +19,30 @@ namespace PuppeteerSharp.Tests.NetworkTests
             Assert.That(request.PostData, Is.EqualTo("{\"foo\":\"bar\"}"));
         }
 
+        [Test, Retry(2), PuppeteerTest("network.spec", "PuppeteerSharp network Request.postData", "should work plain text")]
+        public async Task ShouldWorkPlainText()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            Server.SetRoute("/post", _ => Task.CompletedTask);
+            var requestTask = Page.WaitForRequestAsync((request) => !TestUtils.IsFavicon(request));
+            await Page.EvaluateExpressionHandleAsync("fetch('./post', { method: 'POST', body: 'Hello, world!'})");
+            var request = await requestTask.WithTimeout();
+            Assert.That(request, Is.Not.Null);
+            Assert.That(request.PostData, Is.EqualTo("Hello, world!"));
+        }
+
+        [Test, Retry(2), PuppeteerTest("network.spec", "PuppeteerSharp network Request.postData", "should work with low surrogate")]
+        public async Task ShouldWorkWithLowSurrogate()
+        {
+            await Page.GoToAsync(TestConstants.EmptyPage);
+            Server.SetRoute("/post", _ => Task.CompletedTask);
+            var requestTask = Page.WaitForRequestAsync((request) => !TestUtils.IsFavicon(request));
+            await Page.EvaluateExpressionHandleAsync("fetch('./post', { method: 'POST', body: 'Hello, world!\uDD71'})");
+            var request = await requestTask.WithTimeout();
+            Assert.That(request, Is.Not.Null);
+            Assert.That(request.PostData, Is.EqualTo("Hello, world!\uFFFD"));
+        }
+
         [Test, Retry(2), PuppeteerTest("network.spec", "network Request.postData", "should be |undefined| when there is no post data")]
         public async Task ShouldBeUndefinedWhenThereIsNoPostData()
         {
