8233abb6a9f9cef1ef2af9790b82e43a55f8a5bd
Darío Kondratiuk <dariokondratiuk@gmail.com>
2025-06-09 14:04:39 -0300

    bump to 137

--- lib/PuppeteerSharp/BrowserData/Chrome.cs
+++ lib/PuppeteerSharp/BrowserData/Chrome.cs
@@ -13,7 +13,7 @@ namespace PuppeteerSharp.BrowserData
         /// <summary>
         /// Default chrome build.
         /// </summary>
-        public static string DefaultBuildId => "136.0.7103.92";
+        public static string DefaultBuildId => "137.0.7151.68";
 
         internal static async Task<string> ResolveBuildIdAsync(ChromeReleaseChannel channel)
             => (await GetLastKnownGoodReleaseForChannel(channel).ConfigureAwait(false)).Version;
