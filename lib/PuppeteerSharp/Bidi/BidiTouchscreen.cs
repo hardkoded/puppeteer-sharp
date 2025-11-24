87f8d6759c12b91359d28221eabb1561707b3e4b
Darío Kondratiuk <dariokondratiuk@gmail.com>
2025-11-24 17:15:31 -0300

    Bidi: Touchscreen support (#3015)
    * Bidi: Touchscreen support
    
    * fix

---
+++ lib/PuppeteerSharp/Bidi/BidiTouchscreen.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading.Tasks;
+using PuppeteerSharp.Helpers;
+using PuppeteerSharp.Input;
+using WebDriverBiDi.Input;
+
+namespace PuppeteerSharp.Bidi;
+
+internal class BidiTouchscreen(BidiPage page) : Touchscreen, IDisposable
+{
+    private readonly PointerSourceActions _touchSource = new()
+    {
+        Parameters = new PointerParameters
+        {
+            PointerType = WebDriverBiDi.Input.PointerType.Touch,
+        },
+    };
+
+    private readonly TaskQueue _actionsQueue = new();
+
+    public override Task TouchStartAsync(decimal x, decimal y)
+    {
+        return _actionsQueue.Enqueue(async () =>
+        {
+            _touchSource.Actions.Add(new PointerMoveAction
+            {
+                X = (long)Math.Round(x),
+                Y = (long)Math.Round(y),
+            });
+            _touchSource.Actions.Add(new PointerDownAction(0));
+
+            await page.BidiMainFrame.BrowsingContext.PerformActionsAsync([_touchSource]).ConfigureAwait(false);
+            _touchSource.Actions.Clear();
+        });
+    }
+
+    public override Task TouchMoveAsync(decimal x, decimal y)
+    {
+        return _actionsQueue.Enqueue(async () =>
+        {
+            _touchSource.Actions.Add(new PointerMoveAction
+            {
+                X = (long)Math.Round(x),
+                Y = (long)Math.Round(y),
+            });
+
+            await page.BidiMainFrame.BrowsingContext.PerformActionsAsync([_touchSource]).ConfigureAwait(false);
+            _touchSource.Actions.Clear();
+        });
+    }
+
+    public override Task TouchEndAsync()
+    {
+        return _actionsQueue.Enqueue(async () =>
+        {
+            _touchSource.Actions.Add(new PointerUpAction(0));
+
+            await page.BidiMainFrame.BrowsingContext.PerformActionsAsync([_touchSource]).ConfigureAwait(false);
+            _touchSource.Actions.Clear();
+        });
+    }
+
+    public void Dispose()
+    {
+        _actionsQueue.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
