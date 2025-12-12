6e9f6feb9dc3140a26e69a9977997779672c6ba8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2025-12-12 17:22:24 -0300

    Bidi Pass some query selector tests (#3042)

--- lib/PuppeteerSharp.Tests/QuerySelectorTests/ElementHandleQuerySelectorAllEvalTests.cs
+++ lib/PuppeteerSharp.Tests/QuerySelectorTests/ElementHandleQuerySelectorAllEvalTests.cs
@@ -9,7 +9,14 @@ namespace PuppeteerSharp.Tests.QuerySelectorTests
         [Test, PuppeteerTest("queryselector.spec", "ElementHandle.$$eval", "should work")]
         public async Task ShouldWork()
         {
-            await Page.SetContentAsync("<html><body><div class='tweet'><div class='like'>100</div><div class='like'>10</div></div></body></html>");
+            await Page.SetContentAsync(TestUtils.Html(@"<html>
+          <body>
+            <div class=""tweet"">
+              <div class=""like"">100</div>
+              <div class=""like"">10</div>
+            </div>
+          </body>
+        </html>"));
             var tweet = await Page.QuerySelectorAsync(".tweet");
             var content = await tweet.QuerySelectorAllHandleAsync(".like")
                 .EvaluateFunctionAsync<string[]>("nodes => nodes.map(n => n.innerText)");
@@ -20,7 +27,7 @@ namespace PuppeteerSharp.Tests.QuerySelectorTests
         public async Task QuerySelectorAllShouldRetrieveContentFromSubtree()
         {
             var htmlContent = "<div class='a'>not-a-child-div</div><div id='myId'><div class='a'>a1-child-div</div><div class='a'>a2-child-div</div></div>";
-            await Page.SetContentAsync(htmlContent);
+            await Page.SetContentAsync(TestUtils.Html(htmlContent));
             var elementHandle = await Page.QuerySelectorAsync("#myId");
             var content = await elementHandle.QuerySelectorAllHandleAsync(".a")
                 .EvaluateFunctionAsync<string[]>("nodes => nodes.map(n => n.innerText)");
@@ -31,7 +38,7 @@ namespace PuppeteerSharp.Tests.QuerySelectorTests
         public async Task QuerySelectorAllShouldNotThrowInCaseOfMissingSelector()
         {
             var htmlContent = "<div class='a'>not-a-child-div</div><div id='myId'></div>";
-            await Page.SetContentAsync(htmlContent);
+            await Page.SetContentAsync(TestUtils.Html(htmlContent));
             var elementHandle = await Page.QuerySelectorAsync("#myId");
             var nodesLength = await elementHandle.QuerySelectorAllHandleAsync(".a")
                 .EvaluateFunctionAsync<int>("nodes => nodes.length");
