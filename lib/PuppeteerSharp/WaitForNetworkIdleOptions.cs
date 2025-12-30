37d7bc2b247541330308165d9feb4ef2237ee09b
Darío Kondratiuk <dariokondratiuk@gmail.com>
2025-12-30 09:47:29 -0300

    Bidi: WaitForNetworkIdle

--- lib/PuppeteerSharp/WaitForNetworkIdleOptions.cs
+++ lib/PuppeteerSharp/WaitForNetworkIdleOptions.cs
@@ -10,5 +10,11 @@ namespace PuppeteerSharp
         /// How long to wait for no network requests in milliseconds, defaults to 500 milliseconds.
         /// </summary>
         public int? IdleTime { get; set; }
+
+        /// <summary>
+        /// Maximum number of concurrent network connections to allow before considering network idle.
+        /// Defaults to 0, meaning network is idle when there are no active connections.
+        /// </summary>
+        public int Concurrency { get; set; }
     }
 }
