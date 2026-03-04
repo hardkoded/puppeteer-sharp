0536c1e8c999411eae668e37fa5e610d15c29d1e
Darío Kondratiuk <dariokondratiuk@gmail.com>
2026-03-03 21:22:45 -0300

    Add missing tests: prerender spec (#3310) (#3347)

---
+++ lib/PuppeteerSharp.Tests/PrerenderTests/PrerenderScreencastTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PuppeteerSharp.Nunit;
+
+namespace PuppeteerSharp.Tests.PrerenderTests;
+
+public class PrerenderScreencastTests : PuppeteerPageBaseTest
+{
+    [Test, PuppeteerTest("prerender.spec", "Prerender", "can screencast")]
+    public async Task CanScreencast()
+    {
+        var screencastFramesReceived = 0;
+        var frameReceived = new TaskCompletionSource<bool>();
+        Page.Client.MessageReceived += async (_, e) =>
+        {
+            if (e.MessageID == "Page.screencastFrame")
+            {
+                Assert.That(e.MessageData.GetProperty("data").GetString(), Is.Not.Null.Or.Empty);
+                Interlocked.Increment(ref screencastFramesReceived);
+                frameReceived.TrySetResult(true);
+
+                await Page.Client.SendAsync("Page.screencastFrameAck", new
+                {
+                    sessionId = e.MessageData.GetProperty("sessionId").GetInt32(),
+                });
+            }
+        };
+
+        await Page.Client.SendAsync("Page.startScreencast", new
+        {
+            format = "png",
+            everyNthFrame = 1,
+        });
+
+        await Page.GoToAsync(TestConstants.ServerUrl + "/prerender/index.html");
+
+        var button = await Page.WaitForSelectorAsync("button");
+        await button.ClickAsync();
+
+        var link = await Page.WaitForSelectorAsync("a");
+        await Task.WhenAll(
+            Page.WaitForNavigationAsync(),
+            link.ClickAsync()
+        );
+
+        var input = await Page.WaitForSelectorAsync("input");
+        await input.TypeAsync("ab", new Input.TypeOptions { Delay = 100 });
+
+        await frameReceived.Task.WaitAsync(TimeSpan.FromSeconds(5));
+
+        await Page.Client.SendAsync("Page.stopScreencast");
+
+        Assert.That(screencastFramesReceived, Is.GreaterThan(0));
+    }
+}
